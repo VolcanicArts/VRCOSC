0807547d7f41c2a92afb7d285e79eabe98002650
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-03-14 19:10:36 +0000

    Create TerminalContainer

---
+++ VRCOSC.Game/Graphics/Containers/Terminal/TerminalContainer.cs
@@ -0,0 +1,33 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+
+namespace VRCOSC.Game.Graphics.Containers.Terminal;
+
+public class TerminalContainer : Container
+{
+    public TerminalContainer()
+    {
+        Children = new Drawable[]
+        {
+            new Box
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                RelativeSizeAxes = Axes.Both,
+                Colour = Colour4.Black
+            },
+            new Container
+            {
+                Name = "Content",
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                RelativeSizeAxes = Axes.Both,
+                Padding = new MarginPadding(10),
+                Children = new Drawable[]
+                {
+                }
+            }
+        };
+    }
+}
--- VRCOSC.Game/MainScreen.cs
+++ VRCOSC.Game/MainScreen.cs
@@ -1,9 +1,11 @@
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Screens;
 using osuTK.Graphics;
 using VRCOSC.Game.Graphics.Containers;
+using VRCOSC.Game.Graphics.Containers.Terminal;
 
 namespace VRCOSC.Game;
 
@@ -21,13 +23,36 @@ public class MainScreen : Screen
                 RelativeSizeAxes = Axes.Both,
                 Colour = Color4.Gray
             },
+            new Container
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                RelativeSizeAxes = Axes.Both,
+                Padding = new MarginPadding
+                {
+                    Top = 10,
+                    Bottom = 10 + footer_height,
+                    Left = 10,
+                    Right = 10
+                },
+                Child = new TerminalContainer
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    RelativeSizeAxes = Axes.Both,
+                    Masking = true,
+                    CornerRadius = 20
+                }
+            },
             new MainScreenFooter
             {
                 Anchor = Anchor.BottomCentre,
                 Origin = Anchor.BottomCentre,
                 RelativeSizeAxes = Axes.X,
-                Height = 50
+                Height = footer_height
             }
         };
     }
+
+    private const float footer_height = 50;
 }
