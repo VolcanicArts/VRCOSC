ffc46ef3e2c56f92020cd22483077cdda2945084
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-03-16 02:22:28 +0000

    Create ComputerStatsModule

---
+++ VRCOSC.Game/Modules/Modules/ComputerStats/ComputerStatsModule.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Threading.Tasks;
+using osu.Framework.Graphics;
+using osu.Framework.Platform;
+using VRCOSC.Game.Graphics;
+
+namespace VRCOSC.Game.Modules.Modules.ComputerStats;
+
+public class ComputerStatsModule : Module
+{
+    public override string Title => "Computer Stats";
+    public override string Description => "Takes system stats and passes them into VRChat";
+    public override string Author => "VolcanicArts";
+    public override Colour4 Colour => VRCOSCColour.PurpleLight;
+    public override ModuleType Type => ModuleType.General;
+    public override double DeltaUpdate => 5000d;
+
+    public ComputerStatsModule(Storage storage)
+        : base(storage)
+    {
+        CreateParameter(ComputerStatsParameter.CPUUsage, "CPU Usage", "The current usage of your CPU from 0 to 1", "/avatar/parameters/CPUUsage");
+        CreateParameter(ComputerStatsParameter.GPUUsage, "GPU Usage", "The current usage of your GPU from 0 to 1", "/avatar/parameters/GPUUsage");
+        CreateParameter(ComputerStatsParameter.RAMUsage, "RAM Usage", "The current usage of your RAM from 0 to 1", "/avatar/parameters/RAMUsage");
+
+        LoadData();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        sendCpuUsage();
+        sendGpuUsage();
+        sendRamUsage();
+    }
+
+    private void sendCpuUsage()
+    {
+        Task.Run(async () =>
+        {
+            var usage = await ComputerStatsProvider.GetCpuUsage();
+            Terminal.Log($"Current CPU Usage: {usage * 100f}%");
+            SendParameter(ComputerStatsParameter.CPUUsage, usage);
+        }).ConfigureAwait(false);
+    }
+
+    private void sendGpuUsage()
+    {
+        Task.Run(async () =>
+        {
+            var usage = await ComputerStatsProvider.GetGpuUsage();
+            Terminal.Log($"Current GPU Usage: {usage * 100f}%");
+            SendParameter(ComputerStatsParameter.GPUUsage, usage);
+        }).ConfigureAwait(false);
+    }
+
+    private void sendRamUsage()
+    {
+        Task.Run(async () =>
+        {
+            var usage = await ComputerStatsProvider.GetRamUsage();
+            Terminal.Log($"Current RAM Usage: {usage * 100f}%");
+            SendParameter(ComputerStatsParameter.RAMUsage, usage);
+        }).ConfigureAwait(false);
+    }
+}
+
+public enum ComputerStatsParameter
+{
+    CPUUsage,
+    GPUUsage,
+    RAMUsage
+}
---
+++ VRCOSC.Game/Modules/Modules/ComputerStats/ComputerStatsProvider.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Threading.Tasks;
+
+namespace VRCOSC.Game.Modules.Modules.ComputerStats;
+
+public static class ComputerStatsProvider
+{
+    public static async Task<float> GetCpuUsage()
+    {
+        try
+        {
+            var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            var _ = cpuCounter.NextValue();
+            await Task.Delay(1000);
+            var usage = cpuCounter.NextValue();
+            return MathF.Round(usage) / 100f;
+        }
+        catch
+        {
+            return 0f;
+        }
+    }
+
+    public static async Task<float> GetGpuUsage()
+    {
+        try
+        {
+            var category = new PerformanceCounterCategory("GPU Engine");
+            var counterNames = category.GetInstanceNames();
+            var gpuCounters = new List<PerformanceCounter>();
+            var usage = 0f;
+
+            foreach (string counterName in counterNames)
+            {
+                if (!counterName.EndsWith("engtype_3D")) continue;
+
+                gpuCounters.AddRange(category.GetCounters(counterName).Where(counter => counter.CounterName == "Utilization Percentage"));
+            }
+
+            gpuCounters.ForEach(x => _ = x.NextValue());
+            await Task.Delay(1000);
+            gpuCounters.ForEach(x => usage += x.NextValue());
+
+            return MathF.Round(usage) / 100f;
+        }
+        catch
+        {
+            return 0f;
+        }
+    }
+
+    public static Task<float> GetRamUsage()
+    {
+        var wmiObject = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+
+        var memoryValues = wmiObject.Get().Cast<ManagementObject>().Select(mo => new
+        {
+            FreePhysicalMemory = double.Parse(mo["FreePhysicalMemory"].ToString() ?? string.Empty),
+            TotalVisibleMemorySize = double.Parse(mo["TotalVisibleMemorySize"].ToString() ?? string.Empty)
+        }).FirstOrDefault();
+
+        if (memoryValues == null) return Task.FromResult(0f);
+
+        var percentageUsage = (float)((memoryValues.TotalVisibleMemorySize - memoryValues.FreePhysicalMemory) / memoryValues.TotalVisibleMemorySize) * 100f;
+        return Task.FromResult(MathF.Round(percentageUsage) / 100f);
+    }
+}
