b9a32019474c3643a78c2f32576220b955b27cf0
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-05-30 01:11:27 +0100

    Create `VRChatInputParameters`

---
+++ VRCOSC.Game/Modules/Frameworks/VRChatInputParameters.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+namespace VRCOSC.Game.Modules.Frameworks;
+
+public enum VRChatInputParameters
+{
+    IsLocal,
+    Viseme,
+    Voice,
+    GestureLeft,
+    GestureRight,
+    GestureLeftWeight,
+    GestureRightWeight,
+    AngularY,
+    VelocityX,
+    VelocityY,
+    VelocityZ,
+    Upright,
+    Grounded,
+    Seated,
+    AFK,
+    TrackingType,
+    VRMode,
+    MuteSelf,
+    InStation
+}
