c1c94b5760b8658f0ab1a2e18f1d7eacd83a6466
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-06-07 01:18:15 +0100

    ClockModule update for future prefab

--- VRCOSC.Game/Modules/Modules/Clock/ClockModule.cs
+++ VRCOSC.Game/Modules/Modules/Clock/ClockModule.cs
@@ -10,7 +10,7 @@ namespace VRCOSC.Game.Modules.Modules.Clock;
 public class ClockModule : Module
 {
     public override string Title => "Clock";
-    public override string Description => "Sends your current local time in 2 different formats";
+    public override string Description => "Sends the time as hours, minutes, and seconds";
     public override string Author => "VolcanicArts";
     public override Colour4 Colour => Colour4.LightBlue.Darken(0.5f);
     public override ModuleType Type => ModuleType.General;
@@ -18,84 +18,55 @@ public class ClockModule : Module
 
     protected override Dictionary<Enum, (string, string, object)> Settings => new()
     {
-        { ClockSetting.SendType, ("Send Type", "Whether to send the clock data for blendtree (-1,1) or as individual digits", ClockSettingSendType.SendAsIndividual) }
+        { ClockSettings.UTC, ("UTC", "Send the time as UTC rather than your local time", false) }
     };
 
     protected override Dictionary<Enum, (string, string, string)> OutputParameters => new()
     {
-        { ClockParameter.HoursTens, ("Hours Tens", "The tens value of the current hour", "/avatar/parameters/HoursTens") },
-        { ClockParameter.HoursUnits, ("Hours Units", "The units value of the current hour", "/avatar/parameters/HoursUnits") },
-        { ClockParameter.MinutesTens, ("Minutes Tens", "The tens value of the current minute", "/avatar/parameters/MinutesTens") },
-        { ClockParameter.MinutesUnits, ("Minutes Units", "The units value of the current minute", "/avatar/parameters/MinutesUnits") },
-        { ClockParameter.HourMapped, ("Hour Mapped", "The current hour mapped between -1 and 1", "/avatar/parameters/HourMapped") },
-        { ClockParameter.MinuteMapped, ("Minute Mapped", "The current minute mapped between -1 and 1", "/avatar/parameters/MinuteMapped") }
+        { ClockParameters.Hours, ("Hour", "The current hour normalised", "/avatar/parameters/ClockHour") },
+        { ClockParameters.Minutes, ("Minute", "The current minute normalised", "/avatar/parameters/ClockMinute") },
+        { ClockParameters.Seconds, ("Second", "The current second normalised", "/avatar/parameters/ClockSecond") },
     };
 
     protected override void OnUpdate()
     {
-        var sendType = GetSettingAs<ClockSettingSendType>(ClockSetting.SendType);
+        var sendAsUTC = GetSettingAs<bool>(ClockSettings.UTC);
 
-        switch (sendType)
+        int hour, minute, second;
+
+        if (sendAsUTC)
         {
-            case ClockSettingSendType.SendForBlendtree:
-                sendForBlendTree();
-                break;
-
-            case ClockSettingSendType.SendAsIndividual:
-                sendAsIndividual();
-                break;
+            hour = DateTime.UtcNow.Hour;
+            minute = DateTime.UtcNow.Minute;
+            second = DateTime.UtcNow.Second;
         }
-    }
+        else
+        {
+            hour = DateTime.Now.Hour;
+            minute = DateTime.Now.Minute;
+            second = DateTime.Now.Second;
+        }
 
-    private void sendForBlendTree()
-    {
-        var hour = DateTime.Now.Hour;
-        var minute = DateTime.Now.Minute;
+        Terminal.Log($"Time is currently: {hour}:{minute}:{second}");
 
-        var hourMapped = MathF.Round(MapBetween(hour, 0, 24, -1, 1), 2);
-        var minuteMapped = MathF.Round(MapBetween(minute, 0, 60, -1, 1), 2);
+        var hourNormalised = (hour % 12f) / 12f;
+        var minuteNormalised = minute / 60f;
+        var secondNormalised = second / 60f;
 
-        Terminal.Log("Hour mapped " + hourMapped);
-        Terminal.Log("Minute mapped " + minuteMapped);
-
-        SendParameter(ClockParameter.HourMapped, hourMapped);
-        SendParameter(ClockParameter.MinuteMapped, minuteMapped);
-    }
-
-    private void sendAsIndividual()
-    {
-        var hour = DateTime.Now.Hour;
-        var minute = DateTime.Now.Minute;
-
-        var hoursSplit = ToDigitArray(hour, 2);
-        var minutesSplit = ToDigitArray(minute, 2);
-
-        Terminal.Log($"Time is currently: {hoursSplit[0]}{hoursSplit[1]}:{minutesSplit[0]}{minutesSplit[1]}");
-
-        SendParameter(ClockParameter.HoursTens, hoursSplit[0]);
-        SendParameter(ClockParameter.HoursUnits, hoursSplit[1]);
-        SendParameter(ClockParameter.MinutesTens, minutesSplit[0]);
-        SendParameter(ClockParameter.MinutesUnits, minutesSplit[1]);
+        SendParameter(ClockParameters.Hours, hourNormalised);
+        SendParameter(ClockParameters.Minutes, minuteNormalised);
+        SendParameter(ClockParameters.Seconds, secondNormalised);
     }
 }
 
-public enum ClockParameter
+public enum ClockParameters
 {
-    HoursUnits,
-    HoursTens,
-    MinutesUnits,
-    MinutesTens,
-    HourMapped,
-    MinuteMapped
+    Hours,
+    Minutes,
+    Seconds,
 }
 
-public enum ClockSetting
+public enum ClockSettings
 {
-    SendType
-}
-
-public enum ClockSettingSendType
-{
-    SendForBlendtree,
-    SendAsIndividual
+    UTC
 }
