a7f010a81fdfdc2bc1b5a7b4f7056eab3f55d3bb
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-06-09 15:07:34 +0100

    Allow background colour customisation in `Checkbox`

--- VRCOSC.Game/Graphics/Containers/UI/Checkbox/Checkbox.cs
+++ VRCOSC.Game/Graphics/Containers/UI/Checkbox/Checkbox.cs
@@ -1,8 +1,9 @@
-﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Input.Events;
 
@@ -13,13 +14,15 @@ public class Checkbox : IconButton
     public BindableBool State { get; } = new();
     public IconUsage IconOn { get; init; } = FontAwesome.Solid.Check;
     public IconUsage IconOff { get; init; } = FontAwesome.Solid.Get(0xf00d);
+    public Colour4 ColourOn { get; init; } = VRCOSCColour.Green;
+    public Colour4 ColourOff { get; init; } = VRCOSCColour.Red;
 
     [BackgroundDependencyLoader]
     private void load()
     {
         State.BindValueChanged(e =>
         {
-            BackgroundColour.Value = e.NewValue ? VRCOSCColour.Green : VRCOSCColour.Red;
+            BackgroundColour.Value = e.NewValue ? ColourOn : ColourOff;
             Icon.Value = e.NewValue ? IconOn : IconOff;
         }, true);
     }
