d9da78777f4f432badcf2658f97e576e4bf66c5c
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-06-10 23:27:35 +0100

    Convert OSC booleans to standard booleans

--- VRCOSC.Game/Graphics/Containers/Screens/ModuleRun/ParameterDisplay.cs
+++ VRCOSC.Game/Graphics/Containers/Screens/ModuleRun/ParameterDisplay.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
+using CoreOSC;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics;
@@ -73,6 +74,9 @@ public class ParameterDisplay : Container
 
     private void addEntry(string key, object value)
     {
+        if (value is OscTrue) value = true;
+        if (value is OscFalse) value = false;
+
         var valueStr = value.ToString() ?? "Invalid Object";
 
         bool successful = false;
