86c682050559b4f9e8208877d2363cef1a140cb8
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-06-18 18:29:27 +0100

    Add support for Squirrel installation

--- VRCOSC.Desktop/Program.cs
+++ VRCOSC.Desktop/Program.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
+using System.Runtime.Versioning;
 using osu.Framework;
 using osu.Framework.Platform;
+using Squirrel;
 using VRCOSC.Game;
 
 namespace VRCOSC.Desktop;
@@ -11,8 +13,31 @@ public static class Program
 {
     public static void Main()
     {
+        initSquirrel();
+
         using GameHost host = Host.GetSuitableDesktopHost(@"VRCOSC");
         using osu.Framework.Game game = new VRCOSCGame();
         host.Run(game);
+    }
+
+    [SupportedOSPlatform("windows")]
+    private static void initSquirrel()
+    {
+        SquirrelAwareApp.HandleEvents(onInitialInstall: OnAppInstall, onAppUninstall: OnAppUninstall, onEveryRun: OnAppRun);
+    }
+
+    private static void OnAppInstall(SemanticVersion version, IAppTools tools)
+    {
+        tools.CreateShortcutForThisExe();
+    }
+
+    private static void OnAppUninstall(SemanticVersion version, IAppTools tools)
+    {
+        tools.RemoveShortcutForThisExe();
+    }
+
+    private static void OnAppRun(SemanticVersion version, IAppTools tools, bool firstRun)
+    {
+        tools.SetProcessAppUserModelId();
     }
 }
