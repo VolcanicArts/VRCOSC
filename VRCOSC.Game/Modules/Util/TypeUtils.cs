85f2ee718f2c771a97e28a0f0fc37d6e687936ea
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-07-06 18:55:00 +0100

    Support all types in `TypeUtils`

--- VRCOSC.Game/Modules/Util/TypeUtils.cs
+++ VRCOSC.Game/Modules/Util/TypeUtils.cs
@@ -11,19 +11,27 @@ public static class TypeUtils
     {
         var typeCode = Type.GetTypeCode(typeof(T));
 
-        switch (typeCode)
+        return typeCode switch
         {
-            case TypeCode.Boolean:
-                return "Bool";
-
-            case TypeCode.Int32:
-                return "Int";
-
-            case TypeCode.Single:
-                return "Float";
-
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
+            TypeCode.Empty => "Null",
+            TypeCode.Object => "Object",
+            TypeCode.DBNull => "DBNull",
+            TypeCode.Boolean => "Bool",
+            TypeCode.Char => "Char",
+            TypeCode.SByte => "Byte",
+            TypeCode.Byte => "UByte",
+            TypeCode.Int16 => "Short",
+            TypeCode.UInt16 => "UShort",
+            TypeCode.Int32 => "Int",
+            TypeCode.UInt32 => "UInt",
+            TypeCode.Int64 => "Long",
+            TypeCode.UInt64 => "ULong",
+            TypeCode.Single => "Float",
+            TypeCode.Double => "Double",
+            TypeCode.Decimal => "Decimal",
+            TypeCode.DateTime => "DateTime",
+            TypeCode.String => "String",
+            _ => throw new ArgumentOutOfRangeException($"Unknown type provided of type {nameof(T)}")
+        };
     }
 }
