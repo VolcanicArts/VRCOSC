6087c391547801c79065010575f6ab14ddb3e8a2
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-07-16 22:43:34 +0100

    Implement tab screen switching

---
+++ VRCOSC.Game/Graphics/Settings/SettingsContainer.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+
+namespace VRCOSC.Game.Graphics.Settings;
+
+public sealed class SettingsContainer : Container
+{
+    public SettingsContainer()
+    {
+        Anchor = Anchor.Centre;
+        Origin = Anchor.Centre;
+        RelativeSizeAxes = Axes.Both;
+    }
+
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        Children = new Drawable[]
+        {
+            new Box
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                RelativeSizeAxes = Axes.Both,
+                Colour = VRCOSCColour.Gray1
+            }
+        };
+    }
+}
--- VRCOSC.Game/VRCOSCGame.cs
+++ VRCOSC.Game/VRCOSCGame.cs
@@ -5,6 +5,8 @@ using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using VRCOSC.Game.Graphics.ModuleListing;
+using VRCOSC.Game.Graphics.Settings;
 using VRCOSC.Game.Graphics.Sidebar;
 using VRCOSC.Game.Graphics.Updater;
 
@@ -14,6 +16,7 @@ namespace VRCOSC.Game;
 public abstract class VRCOSCGame : VRCOSCGameBase
 {
     private DependencyContainer dependencies;
+    private Container screenHolder = null!;
 
     public Bindable<Tabs> SelectedTab = new();
 
@@ -34,10 +37,39 @@ public abstract class VRCOSCGame : VRCOSCGameBase
             {
                 new Drawable[]
                 {
-                    new Sidebar()
+                    new Sidebar(),
+                    screenHolder = new Container
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        RelativeSizeAxes = Axes.Both,
+                        Children = new Drawable[]
+                        {
+                            new ModuleListingContainer(),
+                            new SettingsContainer()
+                        }
+                    }
                 }
             }
         };
+    }
+
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+
+        SelectedTab.BindValueChanged(tab =>
+        {
+            var id = (int)tab.NewValue;
+
+            for (int i = 0; i < screenHolder.Count; i++)
+            {
+                if (id == i)
+                    screenHolder[i].Show();
+                else
+                    screenHolder[i].Hide();
+            }
+        }, true);
     }
 
     protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
