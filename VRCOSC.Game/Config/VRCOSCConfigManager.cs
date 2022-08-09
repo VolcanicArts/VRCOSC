a243dbee221b6988d2116c11b3ab047e42e7e963
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-08-09 23:53:34 +0100

    Dropdowns storage can be removed

--- VRCOSC.Game/Config/VRCOSCConfigManager.cs
+++ VRCOSC.Game/Config/VRCOSCConfigManager.cs
@@ -1,10 +1,8 @@
 ﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
-using System;
 using osu.Framework.Configuration;
 using osu.Framework.Platform;
-using VRCOSC.Game.Modules;
 
 namespace VRCOSC.Game.Config;
 
@@ -23,7 +21,6 @@ public class VRCOSCConfigManager : IniConfigManager<VRCOSCSetting>
         SetDefault(VRCOSCSetting.IPAddress, "127.0.0.1");
         SetDefault(VRCOSCSetting.SendPort, 9000);
         SetDefault(VRCOSCSetting.ReceivePort, 9001);
-        SetDefault(VRCOSCSetting.Dropdowns, (int)((Math.Pow(2, Enum.GetValues(typeof(ModuleType)).Length) - 1)));
     }
 }
 
@@ -32,6 +29,5 @@ public enum VRCOSCSetting
     AutoStartStop,
     IPAddress,
     SendPort,
-    ReceivePort,
-    Dropdowns
+    ReceivePort
 }
