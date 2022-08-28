1e5b2ba0065f1bfb78b7be9160aa41399aaadb42
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-08-28 15:20:41 +0100

    Apply missing license headers

--- VRCOSC.Game/Modules/Modules/Heartrate/Pulsoid/Models/PulsoidResponse.cs
+++ VRCOSC.Game/Modules/Modules/Heartrate/Pulsoid/Models/PulsoidResponse.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using Newtonsoft.Json;
 
 namespace VRCOSC.Game.Modules.Modules.Heartrate.Pulsoid.Models;
 
--- VRCOSC.Game/Modules/Modules/Heartrate/Pulsoid/PulsoidModule.cs
+++ VRCOSC.Game/Modules/Modules/Heartrate/Pulsoid/PulsoidModule.cs
@@ -1,4 +1,7 @@
-﻿using osu.Framework.Graphics;
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Colour;
 
 namespace VRCOSC.Game.Modules.Modules.Heartrate.Pulsoid;
