6a150862fe4a83580518b35a5a04978c3c13c5ad
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-09-20 16:20:56 +0100

    Avoid writing to temp file

--- VRCOSC.Game/Modules/Modules/SpeechToText/SpeechToTextModule.cs
+++ VRCOSC.Game/Modules/Modules/SpeechToText/SpeechToTextModule.cs
@@ -10,11 +10,13 @@ namespace VRCOSC.Game.Modules.Modules.SpeechToText;
 public sealed class SpeechToTextModule : Module
 {
     public override string Title => "Speech To Text";
-    public override string Description => "Speech to text for VRChat's ChatBox";
+    public override string Description => "Speech to text using Google's API for VRChat's ChatBox";
     public override string Author => "VolcanicArts";
     public override ModuleType ModuleType => ModuleType.Accessibility;
 
     private readonly SpeechRecognitionEngine speechRecognitionEngine = new();
+    private SpeechClient speechClient;
+    private RecognitionConfig recognitionConfig;
 
     public SpeechToTextModule()
     {
@@ -24,11 +26,7 @@ public sealed class SpeechToTextModule : Module
 
     protected override void CreateAttributes()
     {
-        CreateSetting(SpeechToTextSetting.AccessToken, "Access Token", "Your access token to access the STT API", string.Empty, "Obtain Access Token", async () =>
-        {
-            SetSetting(SpeechToTextSetting.AccessToken, string.Empty);
-            authenticate();
-        });
+        CreateSetting(SpeechToTextSetting.AccessToken, "Access Token", "Your access token to access the STT API", string.Empty, "Obtain Access Token", authenticate);
     }
 
     protected override void OnStart()
@@ -36,6 +34,18 @@ public sealed class SpeechToTextModule : Module
         speechRecognitionEngine.SpeechHypothesized += speechHypothesising;
         speechRecognitionEngine.SpeechRecognized += speechRecognising;
         speechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+
+        var speechBuilder = new SpeechClientBuilder
+        {
+            GoogleCredential = GoogleCredential.FromAccessToken(GetSetting<string>(SpeechToTextSetting.AccessToken))
+        };
+
+        speechClient = speechBuilder.Build();
+
+        recognitionConfig = new RecognitionConfig
+        {
+            LanguageCode = LanguageCodes.English.UnitedStates
+        };
 
         SetChatBoxTyping(false);
         SetChatBoxText("SpeechToText Activated", true);
@@ -60,31 +70,10 @@ public sealed class SpeechToTextModule : Module
     {
         if (e.Result.Audio is null) return;
 
-        var speechBuilder = new SpeechClientBuilder
-        {
-            GoogleCredential = GoogleCredential.FromAccessToken(GetSetting<string>(SpeechToTextSetting.AccessToken))
-        };
-
-        var speech = speechBuilder.Build();
-
-        var config = new RecognitionConfig
-        {
-            LanguageCode = LanguageCodes.English.UnitedStates
-        };
-
-        const string path = @"./tempAudio.wav";
-
-        using (Stream outputStream = new FileStream(path, FileMode.Create))
-        {
-            RecognizedAudio nameAudio = e.Result.Audio;
-            nameAudio.WriteToWaveStream(outputStream);
-            outputStream.Close();
-        }
-
-        var audio = RecognitionAudio.FromFile(@"./tempAudio.wav");
-
-        var response = speech.Recognize(config, audio);
-
+        using var memoryStream = new MemoryStream();
+        e.Result.Audio.WriteToWaveStream(memoryStream);
+        var audio = RecognitionAudio.FromBytes(memoryStream.GetBuffer());
+        var response = speechClient.Recognize(recognitionConfig, audio);
         var text = response.Results[0].Alternatives[0].Transcript;
 
         Terminal.Log($"Recognised: {text}");
