c120d6d49f99476ccbb1fc4dc567e8b884ca8310
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-10-14 09:37:39 +0100

    Replace hardware library and update `HardwareStatsModule`
    Reduces memory allocations and GC runs to virtually nothing

--- VRCOSC.Game/Modules/Modules/HardwareStats/HardwareStatsModule.cs
+++ VRCOSC.Game/Modules/Modules/HardwareStats/HardwareStatsModule.cs
@@ -1,6 +1,8 @@
 // Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
+using System;
+
 namespace VRCOSC.Game.Modules.Modules.HardwareStats;
 
 public sealed class HardwareStatsModule : Module
@@ -9,12 +11,15 @@ public sealed class HardwareStatsModule : Module
     public override string Description => "Sends your hardware stats";
     public override string Author => "VolcanicArts";
     public override ModuleType ModuleType => ModuleType.General;
-    protected override int DeltaUpdate => 5000;
+    protected override int DeltaUpdate => 2000;
 
-    private HardwareStatsProvider hardwareStatsProvider = null!;
+    private HardwareStatsProvider? hardwareStatsProvider;
 
     protected override void CreateAttributes()
     {
+        CreateSetting(HardwareStatsSetting.UseChatBox, "Use ChatBox", "Enable HardwareStats to show values in the ChatBox", true);
+        CreateSetting(HardwareStatsSetting.ChatBoxFormat, "ChatBox Format", "How the information should be displayed in the ChatBox", "CPU: $cpuusage$% | GPU: $gpuusage$% | RAM: $ramusage$%");
+
         CreateOutgoingParameter(HardwareStatsOutgoingParameter.CPUUsage, "CPU Usage", "CPU usage 0-1", "/avatar/parameters/VRCOSC/Hardware/CPUUsage");
         CreateOutgoingParameter(HardwareStatsOutgoingParameter.GPUUsage, "GPU Usage", "GPU usage 0-1", "/avatar/parameters/VRCOSC/Hardware/GPUUsage");
         CreateOutgoingParameter(HardwareStatsOutgoingParameter.RAMUsage, "RAM Usage", "RAM usage 0-1", "/avatar/parameters/VRCOSC/Hardware/RAMUsage");
@@ -23,18 +28,37 @@ public sealed class HardwareStatsModule : Module
     protected override void OnStart()
     {
         hardwareStatsProvider = new HardwareStatsProvider();
+        Log("Loading hardware monitors...");
     }
 
     protected override void OnUpdate()
     {
-        SendParameter(HardwareStatsOutgoingParameter.CPUUsage, hardwareStatsProvider.GetCpuUsage());
-        SendParameter(HardwareStatsOutgoingParameter.GPUUsage, hardwareStatsProvider.GetGpuUsage());
-        SendParameter(HardwareStatsOutgoingParameter.RAMUsage, hardwareStatsProvider.GetRamUsage());
+        if (hardwareStatsProvider is null) throw new NullReferenceException();
+
+        if (!hardwareStatsProvider.CanAcceptQueries) return;
+
+        hardwareStatsProvider.Update();
+
+        SendParameter(HardwareStatsOutgoingParameter.CPUUsage, hardwareStatsProvider.CpuUsage);
+        SendParameter(HardwareStatsOutgoingParameter.GPUUsage, hardwareStatsProvider.GpuUsage);
+        SendParameter(HardwareStatsOutgoingParameter.RAMUsage, hardwareStatsProvider.RamUsage);
+
+        if (GetSetting<bool>(HardwareStatsSetting.UseChatBox)) updateChatBox();
+    }
+
+    private void updateChatBox()
+    {
+        var text = GetSetting<string>(HardwareStatsSetting.ChatBoxFormat)
+                   .Replace("$cpuusage$", (hardwareStatsProvider!.CpuUsage).ToString("#.##"))
+                   .Replace("$gpuusage$", (hardwareStatsProvider!.GpuUsage).ToString("#.##"))
+                   .Replace("$ramusage$", (hardwareStatsProvider!.RamUsage).ToString("#.##"));
+
+        SetChatBoxText(text);
     }
 
     protected override void OnStop()
     {
-        hardwareStatsProvider.Dispose();
+        hardwareStatsProvider = null;
     }
 
     private enum HardwareStatsOutgoingParameter
@@ -42,5 +66,11 @@ public sealed class HardwareStatsModule : Module
         CPUUsage,
         GPUUsage,
         RAMUsage
+    }
+
+    private enum HardwareStatsSetting
+    {
+        UseChatBox,
+        ChatBoxFormat
     }
 }
--- VRCOSC.Game/Modules/Modules/HardwareStats/HardwareStatsProvider.cs
+++ VRCOSC.Game/Modules/Modules/HardwareStats/HardwareStatsProvider.cs
@@ -1,90 +1,73 @@
 ﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Management;
 using System.Threading.Tasks;
-using osu.Framework.Extensions.IEnumerableExtensions;
+using LibreHardwareMonitor.Hardware;
 
 namespace VRCOSC.Game.Modules.Modules.HardwareStats;
 
-public sealed class HardwareStatsProvider : IDisposable
+public sealed class HardwareStatsProvider
 {
-    private PerformanceCounter? cpuUsageProvider;
-    private IEnumerable<PerformanceCounter>? gpuUsageProviders;
-    private ManagementObjectSearcher? ramUsageProvider;
+    private readonly Computer computer;
+
+    public bool CanAcceptQueries { get; private set; }
 
     public HardwareStatsProvider()
     {
+        computer = new Computer
+        {
+            IsCpuEnabled = true,
+            IsGpuEnabled = true,
+            IsMemoryEnabled = true
+        };
+
         Task.Run(() =>
         {
-            initCpu();
-            initGpu();
-            initRam();
+            computer.Open();
+            CanAcceptQueries = true;
         });
     }
 
-    private void initCpu()
+    public void Update()
     {
-        cpuUsageProvider = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-    }
-
-    private void initGpu()
-    {
-        var localGpuUsageProviders = new List<PerformanceCounter>();
-
-        var category = new PerformanceCounterCategory("GPU Engine");
-
-        foreach (string counterName in category.GetInstanceNames().Where(counterName => counterName.EndsWith("engtype_3D")))
+        foreach (IHardware hardware in computer.Hardware)
         {
-            localGpuUsageProviders.AddRange(category.GetCounters(counterName).Where(counter => counter.CounterName == "Utilization Percentage"));
+            hardware.Update();
+
+            foreach (IHardware subHardware in hardware.SubHardware)
+            {
+                foreach (ISensor sensor in subHardware.Sensors)
+                {
+                    handleSensor(sensor);
+                }
+            }
+
+            foreach (ISensor sensor in hardware.Sensors)
+            {
+                handleSensor(sensor);
+            }
         }
-
-        gpuUsageProviders = localGpuUsageProviders;
     }
 
-    private void initRam()
+    private void handleSensor(ISensor sensor)
     {
-        ramUsageProvider = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
-    }
-
-    public float GetCpuUsage()
-    {
-        if (cpuUsageProvider is null) return 0f;
-
-        return cpuUsageProvider.NextValue() / 100f;
-    }
-
-    public float GetGpuUsage()
-    {
-        if (gpuUsageProviders is null) return 0f;
-
-        return gpuUsageProviders.Aggregate(0f, (current, gpuUsageProvider) => current + gpuUsageProvider.NextValue()) / 100f;
-    }
-
-    public float GetRamUsage()
-    {
-        if (ramUsageProvider is null) return 0f;
-
-        var memoryValues = ramUsageProvider.Get().Cast<ManagementObject>().Select(mo => new
+        switch (sensor.Name)
         {
-            FreePhysicalMemory = double.Parse(mo["FreePhysicalMemory"].ToString() ?? "0"),
-            TotalVisibleMemorySize = double.Parse(mo["TotalVisibleMemorySize"].ToString() ?? "0")
-        }).FirstOrDefault();
+            case "CPU Total":
+                CpuUsage = sensor.Value ?? 0f;
+                break;
 
-        if (memoryValues is null) return 0f;
+            case "D3D 3D":
+                GpuUsage = sensor.Value ?? 0f;
+                break;
 
-        return (float)((memoryValues.TotalVisibleMemorySize - memoryValues.FreePhysicalMemory) / memoryValues.TotalVisibleMemorySize);
+            case "Memory Used":
+                RamUsage = sensor.Value ?? 0f;
+                break;
+        }
     }
 
-    public void Dispose()
-    {
-        cpuUsageProvider?.Dispose();
-        gpuUsageProviders?.ForEach(gpuUsageProvider => gpuUsageProvider.Dispose());
-        ramUsageProvider?.Dispose();
-        GC.SuppressFinalize(this);
-    }
+    public float CpuUsage { get; private set; }
+    public float GpuUsage { get; private set; }
+    public float RamUsage { get; private set; }
 }
