5ccb9bfe463f639a7b428ffbec9d01024f49fa32
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-10-20 13:52:26 +0100

    Add game test scene

---
+++ VRCOSC.Game.Tests/Visual/VRCOSCGame.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using NUnit.Framework;
+using VRCOSC.Desktop;
+
+namespace VRCOSC.Game.Tests.Visual;
+
+public class VRCOSCGame : VRCOSCTestScene
+{
+    [SetUp]
+    public void SetUp()
+    {
+        AddGame(new VRCOSCGameDesktop());
+    }
+}
