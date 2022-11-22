b4c04e58ccdcb9b92624f48324a8aed8f89dc82e
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-11-22 00:03:22 +0000

    Cleanup game class

--- VRCOSC.Game/VRCOSCGame.cs
+++ VRCOSC.Game/VRCOSCGame.cs
@@ -14,11 +14,16 @@ using VRCOSC.Game.Graphics.TabBar;
 using VRCOSC.Game.Graphics.Updater;
 using VRCOSC.Game.Modules;
 
+// ReSharper disable InconsistentNaming
+
 namespace VRCOSC.Game;
 
 [Cached]
 public abstract class VRCOSCGame : VRCOSCGameBase
 {
+    private const string latest_release_url = "https://github.com/volcanicarts/vrcosc/releases/latest";
+    private const string discord_invite_url = "https://discord.gg/vj4brHyvT5";
+
     [Cached]
     private ModuleManager moduleManager = new();
 
@@ -65,43 +70,48 @@ public abstract class VRCOSCGame : VRCOSCGameBase
     {
         base.LoadComplete();
 
+        checkUpdates();
+        checkVersion();
+
+        notificationContainer.Notify(new TimedNotification
+        {
+            Title = "Join The Community!",
+            Description = "Click to join the Discord server",
+            Icon = FontAwesome.Brands.Discord,
+            Colour = Colour4.FromHex(@"7289DA"),
+            ClickCallback = () => host.OpenUrlExternally(discord_invite_url),
+            Delay = 5000d
+        });
+
         ModulesRunning.BindValueChanged(e =>
         {
             if (e.NewValue) SelectedTab.Value = Tab.Modules;
         }, true);
+    }
 
+    private void checkUpdates()
+    {
         var updateMode = ConfigManager.Get<UpdateMode>(VRCOSCSetting.UpdateMode);
-
         if (updateMode != UpdateMode.Off) UpdateManager.CheckForUpdate(ConfigManager.Get<string>(VRCOSCSetting.UpdateRepo));
+    }
 
+    private void checkVersion()
+    {
         var lastVersion = ConfigManager.Get<string>(VRCOSCSetting.Version);
 
-        if (Version != lastVersion)
+        if (Version != lastVersion && !string.IsNullOrEmpty(lastVersion))
         {
-            if (!string.IsNullOrEmpty(lastVersion))
+            notificationContainer.Notify(new BasicNotification
             {
-                notificationContainer.Notify(new BasicNotification
-                {
-                    Title = "VRCOSC Updated",
-                    Description = "Click to see the changes",
-                    Icon = FontAwesome.Solid.Download,
-                    Colour = VRCOSCColour.GreenDark,
-                    ClickCallback = () => host.OpenUrlExternally("https://github.com/VolcanicArts/VRCOSC/releases/latest"),
-                });
-            }
+                Title = "VRCOSC Updated",
+                Description = "Click to see the changes",
+                Icon = FontAwesome.Solid.Download,
+                Colour = VRCOSCColour.GreenDark,
+                ClickCallback = () => host.OpenUrlExternally(latest_release_url),
+            });
         }
 
         ConfigManager.SetValue(VRCOSCSetting.Version, Version);
-
-        notificationContainer.Notify(new TimedNotification
-        {
-            Title = "Discord Server",
-            Description = "Click to join the Discord server",
-            Icon = FontAwesome.Brands.Discord,
-            Colour = Colour4.FromHex(@"7289DA"),
-            ClickCallback = () => host.OpenUrlExternally("https://discord.gg/vj4brHyvT5"),
-            Delay = 5000d
-        });
     }
 
     protected override bool OnExiting()
