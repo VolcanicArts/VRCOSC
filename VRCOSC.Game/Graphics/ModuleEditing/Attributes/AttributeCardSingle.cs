e77145d9e6d02ec19a57cc8d49cd7e2eedcedf2a
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-12-08 20:41:47 +0000

    Fix performance degradation when using text attributes

--- VRCOSC.Game/Graphics/ModuleEditing/Attributes/AttributeCardSingle.cs
+++ VRCOSC.Game/Graphics/ModuleEditing/Attributes/AttributeCardSingle.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
+using System;
 using osu.Framework.Bindables;
 using VRCOSC.Game.Modules;
 
@@ -9,16 +10,32 @@ namespace VRCOSC.Game.Graphics.ModuleEditing.Attributes;
 public abstract partial class AttributeCardSingle : AttributeCard
 {
     protected readonly ModuleAttributeSingle AttributeData;
+    protected virtual bool ShouldLimitSaves => false;
+
+    private DateTimeOffset lastUpdateTime;
+    private readonly TimeSpan timeUntilSave = TimeSpan.FromSeconds(0.5f);
+
+    private object lastValue = null!;
 
     protected AttributeCardSingle(ModuleAttributeSingle attributeData)
         : base(attributeData)
     {
         AttributeData = attributeData;
+        lastUpdateTime = DateTimeOffset.Now;
     }
 
     protected override void LoadComplete()
     {
         AttributeData.Attribute.BindValueChanged(e => Schedule(performAttributeUpdate, e), true);
+        lastValue = AttributeData.Attribute.Value;
+    }
+
+    protected override void Update()
+    {
+        if (lastUpdateTime + timeUntilSave < DateTimeOffset.Now && !AttributeData.Attribute.Value.Equals(lastValue))
+        {
+            AttributeData.Attribute.Value = lastValue;
+        }
     }
 
     private void performAttributeUpdate(ValueChangedEvent<object> e)
@@ -29,7 +46,12 @@ public abstract partial class AttributeCardSingle : AttributeCard
 
     protected virtual void UpdateValues(object value)
     {
-        AttributeData.Attribute.Value = value;
+        lastValue = value;
+
+        if (ShouldLimitSaves)
+            lastUpdateTime = DateTimeOffset.Now;
+        else
+            AttributeData.Attribute.Value = value;
     }
 
     protected override void Dispose(bool isDisposing)
