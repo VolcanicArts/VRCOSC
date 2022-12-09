d28c4591163a1ae637be3e71e58352b44c18b53a
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-12-09 22:57:01 +0000

    Allow thresholds to be adjusted in module settings

--- VRCOSC.Game/Modules/Modules/OpenVR/GestureExtensionsModule.cs
+++ VRCOSC.Game/Modules/Modules/OpenVR/GestureExtensionsModule.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
+using System.Threading;
 using System.Threading.Tasks;
 using VRCOSC.Game.Modules.Util;
 
@@ -14,13 +15,24 @@ public class GestureExtensionsModule : Module
     public override ModuleType ModuleType => ModuleType.OpenVR;
     protected override int DeltaUpdate => vrc_osc_delta_update;
 
-    private const float upper_threshold = 0.5f;
-    private const float lower_threshold = 0.5f;
+    private float lowerThreshold;
+    private float upperThreshold;
 
     protected override void CreateAttributes()
     {
+        CreateSetting(GestureExtensionsSetting.LowerThreshold, "Lower Threshold", "How far down a finger should be until it's not considered up", 0.5f, 0, 1);
+        CreateSetting(GestureExtensionsSetting.UpperThreshold, "Upper Threshold", "How far down a finger should be before it's considered down", 0.5f, 0, 1);
+
         CreateParameter<int>(GestureExtensionsParameter.GestureLeft, ParameterMode.Write, "VRCOSC/Gestures/Left", "Custom left hand gesture value");
         CreateParameter<int>(GestureExtensionsParameter.GestureRight, ParameterMode.Write, "VRCOSC/Gestures/Right", "Custom right hand gesture value");
+    }
+
+    protected override Task OnStart(CancellationToken cancellationToken)
+    {
+        lowerThreshold = GetSetting<float>(GestureExtensionsSetting.LowerThreshold);
+        upperThreshold = GetSetting<float>(GestureExtensionsSetting.UpperThreshold);
+
+        return Task.CompletedTask;
     }
 
     protected override Task OnUpdate()
@@ -36,7 +48,7 @@ public class GestureExtensionsModule : Module
     private GestureNames getLeftControllerGesture() => getControllerGesture(OpenVrInterface.LeftController);
     private GestureNames getRightControllerGesture() => getControllerGesture(OpenVrInterface.RightController);
 
-    private static GestureNames getControllerGesture(ControllerData controllerData)
+    private GestureNames getControllerGesture(ControllerData controllerData)
     {
         if (isGestureDoubleGun(controllerData)) return GestureNames.DoubleGun;
         if (isGestureMiddleFinger(controllerData)) return GestureNames.MiddleFinger;
@@ -45,9 +57,36 @@ public class GestureExtensionsModule : Module
         return GestureNames.None;
     }
 
-    private static bool isGestureDoubleGun(ControllerData controllerData) => controllerData is { IndexFinger: < lower_threshold, MiddleFinger: < lower_threshold, RingFinger: > upper_threshold, PinkyFinger: > upper_threshold, ThumbDown: false };
-    private static bool isGestureMiddleFinger(ControllerData controllerData) => controllerData is { IndexFinger: > upper_threshold, MiddleFinger: < lower_threshold, RingFinger: > upper_threshold, PinkyFinger: > upper_threshold };
-    private static bool isGesturePinkyFinger(ControllerData controllerData) => controllerData is { IndexFinger: > upper_threshold, MiddleFinger: > upper_threshold, RingFinger: > upper_threshold, PinkyFinger: < lower_threshold };
+    private bool isGestureDoubleGun(ControllerData controllerData)
+    {
+        return controllerData.IndexFinger < lowerThreshold
+               && controllerData.MiddleFinger < lowerThreshold
+               && controllerData.RingFinger > upperThreshold
+               && controllerData.PinkyFinger > upperThreshold
+               && !controllerData.ThumbDown;
+    }
+
+    private bool isGestureMiddleFinger(ControllerData controllerData)
+    {
+        return controllerData.IndexFinger > upperThreshold
+               && controllerData.MiddleFinger < lowerThreshold
+               && controllerData.RingFinger > upperThreshold
+               && controllerData.PinkyFinger > upperThreshold;
+    }
+
+    private bool isGesturePinkyFinger(ControllerData controllerData)
+    {
+        return controllerData.IndexFinger > upperThreshold
+               && controllerData.MiddleFinger > upperThreshold
+               && controllerData.RingFinger > upperThreshold
+               && controllerData.PinkyFinger < lowerThreshold;
+    }
+
+    private enum GestureExtensionsSetting
+    {
+        LowerThreshold,
+        UpperThreshold
+    }
 
     private enum GestureNames
     {
