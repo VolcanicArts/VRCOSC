4bd53c819b47622fd831803a0927ac0f8aa87d35
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-12-12 18:01:30 +0000

    Remove logging from `BaseWebSocket`

--- VRCOSC.Game/Modules/BaseWebSocket.cs
+++ VRCOSC.Game/Modules/BaseWebSocket.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using SuperSocket.ClientEngine;
 using WebSocket4Net;
 
 // ReSharper disable MemberCanBeProtected.Global
@@ -14,7 +13,6 @@ namespace VRCOSC.Game.Modules;
 public class BaseWebSocket : IDisposable
 {
     private readonly CancellationTokenSource tokenSource = new();
-    private readonly TerminalLogger terminal = new(nameof(BaseWebSocket));
     private readonly WebSocket webSocket;
 
     public Action? OnWsConnected;
@@ -23,12 +21,10 @@ public class BaseWebSocket : IDisposable
 
     public BaseWebSocket(string uri)
     {
-        terminal.Log("Creating base websocket");
         webSocket = new WebSocket(uri);
         webSocket.Opened += wsConnected;
         webSocket.Closed += wsDisconnected;
         webSocket.MessageReceived += wsMessageReceived;
-        webSocket.Error += wsError;
     }
 
     public void Connect()
@@ -52,13 +48,11 @@ public class BaseWebSocket : IDisposable
 
     private void wsConnected(object? sender, EventArgs e)
     {
-        terminal.Log("WebSocket successfully connected");
         OnWsConnected?.Invoke();
     }
 
     private void wsDisconnected(object? sender, EventArgs e)
     {
-        terminal.Log("WebSocket disconnected");
         OnWsDisconnected?.Invoke();
         tokenSource.Cancel();
     }
@@ -66,11 +60,6 @@ public class BaseWebSocket : IDisposable
     private void wsMessageReceived(object? sender, MessageReceivedEventArgs e)
     {
         OnWsMessage?.Invoke(e.Message);
-    }
-
-    private void wsError(object? sender, ErrorEventArgs e)
-    {
-        terminal.Log(e.Exception.ToString());
     }
 
     public virtual void Dispose()
