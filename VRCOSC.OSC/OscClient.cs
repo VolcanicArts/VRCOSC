b79464f561df2be5c22f18e8026049b1f2c4bc12
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-12-12 00:20:19 +0000

    Ensure data sent is surrounded by a lock

--- VRCOSC.OSC/OscClient.cs
+++ VRCOSC.OSC/OscClient.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
 using System.Net;
@@ -81,7 +81,11 @@ public sealed class OscClient
     {
         data.PreValidate();
         sendingClient?.SendOscMessage(new OscMessage(data.Address, data.Values));
-        listeners.ForEach(listener => listener.OnDataSent(data));
+
+        lock (listenerLock)
+        {
+            listeners.ForEach(listener => listener.OnDataSent(data));
+        }
     }
 
     private async void runReceiveLoop()
