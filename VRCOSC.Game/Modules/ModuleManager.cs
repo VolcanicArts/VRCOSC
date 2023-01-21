b7052ad8f23ab79ea8e130cd59554cb83185157a
VolcanicArts <VolcanicArtsBP@Gmail.com>
2023-01-21 14:23:24 +0000

    Support external modules

--- VRCOSC.Game/Modules/ModuleManager.cs
+++ VRCOSC.Game/Modules/ModuleManager.cs
@@ -4,10 +4,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Platform;
 using VRCOSC.Game.Modules.Modules.ChatBoxText;
 using VRCOSC.Game.Modules.Modules.Clock;
 using VRCOSC.Game.Modules.Modules.Discord;
@@ -43,6 +46,9 @@ public sealed partial class ModuleManager : CompositeComponent, IEnumerable<Modu
 
     private readonly TerminalLogger terminal = new(nameof(ModuleManager));
 
+    [Resolved]
+    private Storage storage { get; set; } = null!;
+
     [BackgroundDependencyLoader]
     private void load()
     {
@@ -52,6 +58,23 @@ public sealed partial class ModuleManager : CompositeComponent, IEnumerable<Modu
             LoadComponent(module);
             AddInternal(module);
         });
+
+        loadExternalModules();
+    }
+
+    private void loadExternalModules()
+    {
+        var moduleDirectoryPath = storage.GetStorageForDirectory("custom").GetFullPath(string.Empty, true);
+        Directory.GetFiles(moduleDirectoryPath, "*.dll").ForEach(createModule);
+    }
+
+    private void createModule(string dllPath)
+    {
+        var moduleAssemblyTypes = Assembly.LoadFile(dllPath).GetTypes();
+        var moduleType = moduleAssemblyTypes.First(type => type.IsSubclassOf(typeof(Module)) && !type.IsAbstract);
+        var moduleInstance = (Module)Activator.CreateInstance(moduleType)!;
+        LoadComponent(moduleInstance);
+        AddInternal(moduleInstance);
     }
 
     public void Start()
