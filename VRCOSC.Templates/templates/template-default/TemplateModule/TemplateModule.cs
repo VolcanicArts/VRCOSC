fe4d4a8085ac360bf94bd727a828e47837abec75
VolcanicArts <VolcanicArtsBP@Gmail.com>
2023-01-22 21:28:57 +0000

    Create templates

---
+++ VRCOSC.Templates/templates/template-default/TemplateModule/TemplateModule.cs
@@ -0,0 +1,41 @@
+﻿using VRCOSC.Game.Modules;
+
+namespace TemplateModule
+{
+    public partial class TemplateModule : Module
+    {
+        public override string Title => "Template";
+        public override string Description => "Template description";
+        public override string Author => "Author";
+        public override ModuleType Type => ModuleType.General;
+        protected override TimeSpan DeltaUpdate => TimeSpan.MaxValue;
+
+        protected override void CreateAttributes()
+        {
+            CreateSetting(TemplateSetting.ExampleSetting, "Example Setting", "An example setting", string.Empty);
+            CreateParameter<bool>(TemplateParameter.ExampleParameter, ParameterMode.ReadWrite, "ExampleParameter", "An example parameter");
+        }
+
+        protected override void OnModuleStart()
+        {
+        }
+
+        protected override void OnModuleUpdate()
+        {
+        }
+
+        protected override void OnModuleStop()
+        {
+        }
+
+        private enum TemplateSetting
+        {
+            ExampleSetting
+        }
+
+        private enum TemplateParameter
+        {
+            ExampleParameter
+        }
+    }
+}
