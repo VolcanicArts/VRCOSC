d83660f880fd4cbf35d27f820cfe20da04d741eb
VolcanicArts <VolcanicArtsBP@Gmail.com>
2023-02-21 18:32:18 +0000

    Show not supported message for any incorrect status

--- VRCOSC.Modules/FaceTracking/SRanipalModule.cs
+++ VRCOSC.Modules/FaceTracking/SRanipalModule.cs
@@ -1,4 +1,4 @@
-// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
 using System.Diagnostics;
@@ -52,7 +52,7 @@ public partial class SRanipalModule : Module
                 Log($"{name} tracking has been initialised and is available");
                 break;
 
-            case Error.NOT_SUPPORTED:
+            default:
                 Log($"{name} tracking is not supported in this session");
                 break;
         }
