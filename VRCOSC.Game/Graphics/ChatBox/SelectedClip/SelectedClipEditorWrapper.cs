2b1cc1e4ee7c62ad015ebd0b9c4e0993472e678d
VolcanicArts <VolcanicArtsBP@Gmail.com>
2023-04-26 18:17:37 +0100

    Adjust available variables width to work better on smaller screens

--- VRCOSC.Game/Graphics/ChatBox/SelectedClip/SelectedClipEditorWrapper.cs
+++ VRCOSC.Game/Graphics/ChatBox/SelectedClip/SelectedClipEditorWrapper.cs
@@ -1,4 +1,4 @@
-// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
 using osu.Framework.Allocation;
@@ -99,7 +99,7 @@ public partial class SelectedClipEditorWrapper : Container
                                     new Dimension(GridSizeMode.Absolute, 5),
                                     new Dimension(),
                                     new Dimension(GridSizeMode.Absolute, 5),
-                                    new Dimension(GridSizeMode.Relative, 0.15f),
+                                    new Dimension(GridSizeMode.Relative, 0.2f),
                                 },
                                 Content = new[]
                                 {
