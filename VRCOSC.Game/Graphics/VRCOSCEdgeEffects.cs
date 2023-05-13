165ed358b1127aafa7c01f3cccd54519314102b6
VolcanicArts <VolcanicArtsBP@Gmail.com>
2023-05-13 14:55:11 +0100

    Improve light theme

--- VRCOSC.Game/Graphics/VRCOSCEdgeEffects.cs
+++ VRCOSC.Game/Graphics/VRCOSCEdgeEffects.cs
@@ -5,6 +5,7 @@ using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics.Effects;
 using osuTK;
 using osuTK.Graphics;
+using VRCOSC.Game.Graphics.Themes;
 
 // ReSharper disable InconsistentNaming
 
@@ -12,7 +13,7 @@ namespace VRCOSC.Game.Graphics;
 
 public static class VRCOSCEdgeEffects
 {
-    public static readonly EdgeEffectParameters NoShadow = new()
+    public static EdgeEffectParameters NoShadow => new()
     {
         Colour = new Color4(0, 0, 0, 0),
         Radius = 0f,
@@ -20,26 +21,26 @@ public static class VRCOSCEdgeEffects
         Offset = Vector2.Zero
     };
 
-    public static readonly EdgeEffectParameters BasicShadow = new()
+    public static EdgeEffectParameters BasicShadow => new()
     {
-        Colour = Color4.Black.Opacity(0.6f),
+        Colour = ThemeManager.Current[ThemeAttribute.Darker].Opacity(0.6f),
         Radius = 2.5f,
         Type = EdgeEffectType.Shadow,
         Offset = new Vector2(0.0f, 1.5f)
     };
 
-    public static readonly EdgeEffectParameters UniformShadow = new()
+    public static EdgeEffectParameters UniformShadow => new()
     {
-        Colour = Color4.Black.Opacity(0.6f),
+        Colour = ThemeManager.Current[ThemeAttribute.Darker].Opacity(0.6f),
         Radius = 5f,
         Type = EdgeEffectType.Shadow,
         Offset = new Vector2(0f),
         Hollow = false
     };
 
-    public static readonly EdgeEffectParameters DispersedShadow = new()
+    public static EdgeEffectParameters DispersedShadow => new()
     {
-        Colour = Color4.Black.Opacity(0.75f),
+        Colour = ThemeManager.Current[ThemeAttribute.Darker].Opacity(0.75f),
         Radius = 15f,
         Type = EdgeEffectType.Shadow,
         Offset = new Vector2(0f)
