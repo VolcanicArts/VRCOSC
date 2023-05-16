c15a674686bc23f6952c576bf3e88281452dc393
VolcanicArts <VolcanicArtsBP@Gmail.com>
2023-05-16 17:33:38 +0100

    Update About screen graphics

---
+++ VRCOSC.Game/Graphics/About/AboutHeader.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using VRCOSC.Game.Config;
+using VRCOSC.Game.Graphics.Screen;
+
+namespace VRCOSC.Game.Graphics.About;
+
+public partial class AboutHeader : BaseHeader
+{
+    [Resolved]
+    private VRCOSCConfigManager configManager { get; set; } = null!;
+
+    private Bindable<string> versionBindable = null!;
+
+    protected override string Title => $"VRCOSC {versionBindable.Value}";
+
+    protected override string SubTitle => "Copyright VolcanicArts 2023. See license file in repository root for more information";
+
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        versionBindable = configManager.GetBindable<string>(VRCOSCSetting.Version);
+        versionBindable.BindValueChanged(_ => GenerateText(), true);
+    }
+}
--- VRCOSC.Game/Graphics/About/AboutScreen.cs
+++ VRCOSC.Game/Graphics/About/AboutScreen.cs
@@ -3,70 +3,48 @@
 
 using System;
 using osu.Framework.Allocation;
-using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Platform;
 using osuTK;
-using VRCOSC.Game.Config;
-using VRCOSC.Game.Graphics.Themes;
+using VRCOSC.Game.Graphics.Screen;
 using VRCOSC.Game.Graphics.UI.Button;
 
 namespace VRCOSC.Game.Graphics.About;
 
-public sealed partial class AboutScreen : Container
+public sealed partial class AboutScreen : BaseScreen
 {
     [Resolved]
     private GameHost host { get; set; } = null!;
 
-    [Resolved]
-    private VRCOSCConfigManager configManager { get; set; } = null!;
+    private FillFlowContainer buttonFlow = null!;
 
-    private readonly FillFlowContainer buttonFlow;
-    private readonly TextFlowContainer text;
-    private Bindable<string> versionBindable = null!;
+    protected override BaseHeader CreateHeader() => new AboutHeader();
 
-    public AboutScreen()
+    protected override Drawable CreateBody() => new Container
     {
-        RelativeSizeAxes = Axes.Both;
-
+        RelativeSizeAxes = Axes.Both,
+        Padding = new MarginPadding(10),
         Children = new Drawable[]
         {
-            new Box
+            buttonFlow = new FillFlowContainer
             {
+                Anchor = Anchor.TopCentre,
+                Origin = Anchor.TopCentre,
                 RelativeSizeAxes = Axes.Both,
-                Colour = ThemeManager.Current[ThemeAttribute.Light]
-            },
-            new Container
-            {
-                RelativeSizeAxes = Axes.Both,
-                Padding = new MarginPadding(10),
-                Children = new Drawable[]
-                {
-                    buttonFlow = new FillFlowContainer
-                    {
-                        Anchor = Anchor.TopCentre,
-                        Origin = Anchor.TopCentre,
-                        RelativeSizeAxes = Axes.Both,
-                        Size = new Vector2(0.5f, 0.9f),
-                        Direction = FillDirection.Full,
-                        Spacing = new Vector2(5)
-                    },
-                    text = new TextFlowContainer(t => t.Colour = ThemeManager.Current[ThemeAttribute.Text])
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                        TextAnchor = Anchor.BottomCentre
-                    }
-                }
+                Size = new Vector2(0.5f, 0.9f),
+                Direction = FillDirection.Full,
+                Spacing = new Vector2(5)
             }
-        };
-    }
+        }
+    };
 
     [BackgroundDependencyLoader]
     private void load()
     {
+        RelativeSizeAxes = Axes.Both;
+
         buttonFlow.AddRange(new[]
         {
             new AboutButton
@@ -82,15 +60,6 @@ public sealed partial class AboutScreen : Container
                 Action = () => host.OpenUrlExternally("https://discord.gg/vj4brHyvT5")
             }
         });
-
-        versionBindable = configManager.GetBindable<string>(VRCOSCSetting.Version);
-
-        versionBindable.BindValueChanged(version =>
-        {
-            text.Clear();
-            text.AddText($"VRCOSC {version.NewValue}");
-            text.AddParagraph("Copyright VolcanicArts 2023. See license file in repository root for more information");
-        }, true);
     }
 
     private sealed partial class AboutButton : Container
