a6d8cb3b0be167df9172d2fa3fb542f5006017d8
VolcanicArts <VolcanicArtsBP@Gmail.com>
2023-06-03 13:35:27 +0100

    Disallow negatives in IntTextBox by default

--- VRCOSC.Game/Graphics/UI/Text/IntTextBox.cs
+++ VRCOSC.Game/Graphics/UI/Text/IntTextBox.cs
@@ -5,12 +5,14 @@ namespace VRCOSC.Game.Graphics.UI.Text;
 
 public partial class IntTextBox : ValidationTextBox<int>
 {
+    public bool AllowNegative { get; init; } = false;
+
     public IntTextBox()
     {
         EmptyIsValid = false;
     }
 
-    protected override bool IsTextValid(string text) => int.TryParse(text, out _);
+    protected override bool IsTextValid(string text) => int.TryParse(text, out var intText) && (intText >= 0 || AllowNegative);
 
     protected override int GetConvertedText() => int.Parse(Current.Value);
 }
