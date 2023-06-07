381ae20208cf97aabeea590f59e18c715ecdd2df
VolcanicArts <VolcanicArtsBP@Gmail.com>
2023-06-07 17:50:56 +0100

    Allow VRCOSC to minimise to tray

--- VRCOSC.Game/VRCOSCGameBase.cs
+++ VRCOSC.Game/VRCOSCGameBase.cs
@@ -3,12 +3,16 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Reflection;
+using System.Windows.Forms;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Configuration;
 using osu.Framework.IO.Stores;
 using osu.Framework.Platform;
+using PInvoke;
 using VRCOSC.Game.Config;
 using VRCOSC.Resources;
 
@@ -36,6 +40,10 @@ public partial class VRCOSCGameBase : osu.Framework.Game
         { FrameworkSetting.FrameSync, FrameSync.VSync }
     };
 
+    private readonly BindableBool isInTray = new();
+    private readonly NotifyIcon trayIcon = new();
+    private IntPtr? windowHandle;
+
     [BackgroundDependencyLoader]
     private void load(Storage storage)
     {
@@ -47,11 +55,56 @@ public partial class VRCOSCGameBase : osu.Framework.Game
         versionBindable.BindValueChanged(version => host.Window.Title = $"{host.Name} {version.NewValue}", true);
 
         Window.WindowState = ConfigManager.Get<WindowState>(VRCOSCSetting.WindowState);
+
+        setupTrayIcon();
+    }
+
+    protected override void Update()
+    {
+        if (host.Window.WindowState == WindowState.Minimised || isInTray.Value)
+            host.DrawThread.InactiveHz = 1;
+        else
+            host.DrawThread.InactiveHz = 60;
     }
 
     protected override bool OnExiting()
     {
         ConfigManager.SetValue(VRCOSCSetting.WindowState, Window.WindowState);
+
+        trayIcon.Dispose();
+
         return false;
+    }
+
+    private void setupTrayIcon()
+    {
+        trayIcon.DoubleClick += (_, _) => handleTrayTransition();
+
+        trayIcon.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+        trayIcon.Visible = true;
+        trayIcon.Text = host.Name;
+
+        var contextMenu = new ContextMenuStrip
+        {
+            Items = { { "Exit", null, (_, _) => host.Exit() } }
+        };
+
+        trayIcon.ContextMenuStrip = contextMenu;
+    }
+
+    private void handleTrayTransition()
+    {
+        var localWindowHandle = Process.GetCurrentProcess().MainWindowHandle;
+
+        if (localWindowHandle != IntPtr.Zero)
+        {
+            windowHandle ??= localWindowHandle;
+        }
+
+        if (windowHandle is not null)
+        {
+            isInTray.Toggle();
+            User32.ShowWindow(windowHandle.Value, isInTray.Value ? User32.WindowShowStyle.SW_HIDE : User32.WindowShowStyle.SW_SHOWDEFAULT);
+        }
     }
 }
