d2eb11e4251329c868b99c96313fc49b0eefb312
VolcanicArts <VolcanicArtsBP@Gmail.com>
2023-08-12 10:05:07 +0100

    Ensure only loaded modules can be migrated

--- VRCOSC.Game/Managers/ModuleManager.cs
+++ VRCOSC.Game/Managers/ModuleManager.cs
@@ -56,7 +56,7 @@ public sealed class ModuleManager : IEnumerable<ModuleCollection>
     public bool DoesModuleExist(string serialisedName) => assemblyContexts.Any(context => context.Assemblies.Any(assembly => assembly.ExportedTypes.Where(type => type.IsSubclassOf(typeof(Module)) && !type.IsAbstract).Any(type => type.Name.ToLowerInvariant() == serialisedName)));
     public bool IsModuleLoaded(string serialisedName) => GetModule(serialisedName) is not null;
     public bool IsModuleEnabled(string serialisedName) => GetModule(serialisedName)?.Enabled.Value ?? false;
-    public List<(string, string)> GetMigrations() => (from module in Modules where module.LegacySerialisedName is not null select (module.LegacySerialisedName, module.SerialisedName)).ToList();
+    public List<(string, string)> GetMigrations() => Modules.Where(module => module.LegacySerialisedName is not null && IsModuleLoaded(module.SerialisedName)).Select(module => (module.LegacySerialisedName!, module.SerialisedName)).ToList();
 
     private void loadModules()
     {
