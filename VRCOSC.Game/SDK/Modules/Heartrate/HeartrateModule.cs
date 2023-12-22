387385a067057119c5077c502b6275d468c5b26c
VolcanicArts <VolcanicArtsBP@Gmail.com>
2023-12-22 16:10:54 +0000

    Update HeartrateModule to support average and beat parameters

--- VRCOSC.Game/SDK/Modules/Heartrate/HeartrateModule.cs
+++ VRCOSC.Game/SDK/Modules/Heartrate/HeartrateModule.cs
@@ -2,9 +2,12 @@
 // See the LICENSE file in the repository root for full license text.
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Utils;
 using VRCOSC.SDK.Avatars;
 using VRCOSC.SDK.Parameters;
@@ -19,10 +22,20 @@ public abstract class HeartrateModule<T> : AvatarModule where T : HeartrateProvi
 
     protected virtual int ReconnectionDelay => 2000;
     protected virtual int ReconnectionLimit => 5;
-
-    private float currentHeartrate;
-    private int targetHeartrate;
     private int connectionCount;
+
+    private float currentValue;
+    private int targetValue;
+
+    private float currentAverage;
+    private int targetAverage;
+
+    private bool beatParameterValue;
+    private CancellationTokenSource? beatParameterSource;
+    private Task? beatParameterTask;
+
+    private object valuesLock = new();
+    private readonly Dictionary<DateTimeOffset, int> values = new();
 
     protected abstract T CreateProvider();
 
@@ -30,40 +43,64 @@ public abstract class HeartrateModule<T> : AvatarModule where T : HeartrateProvi
 
     protected override void OnLoad()
     {
-        CreateToggle(HeartrateSetting.Smoothing, "Smoothing", "Whether the current heartrate should jump or smoothly converge to the received heartrate", true);
-        CreateTextBox(HeartrateSetting.SmoothingLength, "Smoothing Length", "The length of time (in milliseconds) the current heartrate should take to converge to the received heartrate", 1000);
+        CreateToggle(HeartrateSetting.SmoothValue, "Smoothing", "Whether the Value parameter should jump or smoothly converge to the received heartrate", true);
+        CreateTextBox(HeartrateSetting.SmoothValueLength, "Smoothing Length", "The length of time (in milliseconds) the Value parameter should take to converge to the received heartrate", 1000);
+
+        CreateTextBox(HeartrateSetting.AveragePeriod, "Average Period", "The period of time (in milliseconds) that should be used to calculate your average heartrate", 10000);
+        CreateToggle(HeartrateSetting.SmoothAverage, "Smoothing", "Whether the Average parameter should jump or smoothly converge to the average heartrate", true);
+        CreateTextBox(HeartrateSetting.SmoothAverageLength, "Smoothing Length", "The length of time (in milliseconds) the Average parameter should take to converge to the average heartrate", 1000);
+
         CreateTextBox(HeartrateSetting.NormalisedLowerbound, "Normalised Lowerbound", "The lower bound BPM the normalised parameter should use", 0);
         CreateTextBox(HeartrateSetting.NormalisedUpperbound, "Normalised Upperbound", "The upper bound BPM the normalised parameter should use", 240);
 
+        CreateToggle(HeartrateSetting.BeatMode, "Beat Mode", "Whether the Beat parameter should toggle its value or become true for 1 update\nFalse for toggle. True for become true", false);
+
         RegisterParameter<bool>(HeartrateParameter.Connected, "VRCOSC/Heartrate/Connected", ParameterMode.Write, "Connected", "Whether this module is connected and receiving values");
         RegisterParameter<int>(HeartrateParameter.Value, "VRCOSC/Heartrate/Value", ParameterMode.Write, "Value", "The value of your heartrate");
-        RegisterParameter<float>(HeartrateParameter.Normalised, "VRCOSC/Heartrate/Normalised", ParameterMode.Write, "Normalised", "The heartrate value normalised to the set bounds");
+        RegisterParameter<float>(HeartrateParameter.Normalised, "VRCOSC/Heartrate/Normalised", ParameterMode.Write, "Normalised", "The heartrate value normalised from the set bounds to 0-1");
+        RegisterParameter<int>(HeartrateParameter.Average, "VRCOSC/Heartrate/Average", ParameterMode.Write, "Average", "The average of your heartrate");
+        RegisterParameter<bool>(HeartrateParameter.Beat, "VRCOSC/Heartrate/Beat", ParameterMode.ReadWrite, "Beat", "Toggles value OR becomes true for 1 update (depending on the setting) when your heart beats");
 
         RegisterParameter<bool>(LegacyHeartrateParameter.Enabled, "VRCOSC/Heartrate/Enabled", ParameterMode.Write, "Legacy: Enabled", "Whether this module is connected and receiving values");
         RegisterParameter<float>(LegacyHeartrateParameter.Units, "VRCOSC/Heartrate/Units", ParameterMode.Write, "Legacy: Units", "The units digit 0-9 mapped to a float");
         RegisterParameter<float>(LegacyHeartrateParameter.Tens, "VRCOSC/Heartrate/Tens", ParameterMode.Write, "Legacy: Tens", "The tens digit 0-9 mapped to a float");
         RegisterParameter<float>(LegacyHeartrateParameter.Hundreds, "VRCOSC/Heartrate/Hundreds", ParameterMode.Write, "Legacy: Hundreds", "The hundreds digit 0-9 mapped to a float");
 
-        CreateGroup("Smoothing", HeartrateSetting.Smoothing, HeartrateSetting.SmoothingLength);
+        CreateGroup("Value", HeartrateSetting.SmoothValue, HeartrateSetting.SmoothValueLength);
+        CreateGroup("Average", HeartrateSetting.AveragePeriod, HeartrateSetting.SmoothAverage, HeartrateSetting.SmoothAverageLength);
+        CreateGroup("Beat", HeartrateSetting.BeatMode);
         CreateGroup("Normalised Parameter", HeartrateSetting.NormalisedLowerbound, HeartrateSetting.NormalisedUpperbound);
     }
 
     protected override void OnPostLoad()
     {
-        GetSetting(HeartrateSetting.SmoothingLength)!.IsEnabled = () => GetSettingValue<bool>(HeartrateSetting.Smoothing);
+        GetSetting(HeartrateSetting.SmoothValueLength)!.IsEnabled = () => GetSettingValue<bool>(HeartrateSetting.SmoothValue);
+        GetSetting(HeartrateSetting.SmoothAverageLength)!.IsEnabled = () => GetSettingValue<bool>(HeartrateSetting.SmoothAverage);
     }
 
     protected override async Task<bool> OnModuleStart()
     {
-        currentHeartrate = 0;
-        targetHeartrate = 0;
+        currentValue = 0;
+        targetValue = 0;
+        currentAverage = 0;
+        targetAverage = 0;
         connectionCount = 1;
+        beatParameterValue = false;
+        values.Clear();
+
+        beatParameterSource = new CancellationTokenSource();
+        beatParameterTask = Task.Run(handleBeatParameter);
 
         HeartrateProvider = CreateProvider();
         HeartrateProvider.OnHeartrateUpdate += newHeartrate =>
         {
             connectionCount = 0;
-            targetHeartrate = newHeartrate;
+            targetValue = newHeartrate;
+
+            lock (valuesLock)
+            {
+                values.Add(DateTimeOffset.Now, newHeartrate);
+            }
         };
         HeartrateProvider.OnDisconnected += attemptReconnection;
         HeartrateProvider.OnLog += Log;
@@ -96,7 +133,12 @@ public abstract class HeartrateModule<T> : AvatarModule where T : HeartrateProvi
         await HeartrateProvider.Initialise();
     }
 
-    protected override Task OnModuleStop() => teardownProvider();
+    protected override async Task OnModuleStop()
+    {
+        beatParameterSource?.Cancel();
+        await (beatParameterTask ?? Task.CompletedTask);
+        await teardownProvider();
+    }
 
     private async Task teardownProvider()
     {
@@ -111,16 +153,79 @@ public abstract class HeartrateModule<T> : AvatarModule where T : HeartrateProvi
         SendParameter(LegacyHeartrateParameter.Enabled, false);
     }
 
-    [ModuleUpdate(ModuleUpdateMode.Custom)]
-    private void updateCurrentHeartrate()
+    private async Task handleBeatParameter()
     {
-        if (GetSettingValue<bool>(HeartrateSetting.Smoothing))
+        while (!beatParameterSource!.IsCancellationRequested)
         {
-            currentHeartrate = (float)Interpolation.DampContinuously(currentHeartrate, targetHeartrate, GetSettingValue<int>(HeartrateSetting.SmoothingLength) / 2d, 50d);
+            if (targetValue == 0f) continue;
+
+            var delay = (int)MathF.Round(60f * 1000f / targetValue);
+            await Task.Delay(delay);
+
+            if (GetSettingValue<bool>(HeartrateSetting.BeatMode))
+            {
+                beatParameterValue = true;
+                SendParameter(HeartrateParameter.Beat, beatParameterValue);
+            }
+            else
+            {
+                beatParameterValue = !beatParameterValue;
+                SendParameter(HeartrateParameter.Beat, beatParameterValue);
+            }
+        }
+    }
+
+    protected override async void OnRegisteredParameterReceived(AvatarParameter avatarParameter)
+    {
+        switch (avatarParameter.Lookup)
+        {
+            case HeartrateParameter.Beat:
+                if (GetSettingValue<bool>(HeartrateSetting.BeatMode) && avatarParameter.GetValue<bool>())
+                {
+                    await Task.Delay(50);
+                    beatParameterValue = false;
+                    SendParameter(HeartrateParameter.Beat, beatParameterValue);
+                }
+
+                break;
+        }
+    }
+
+    [ModuleUpdate(ModuleUpdateMode.Custom)]
+    private void updateValue()
+    {
+        if (GetSettingValue<bool>(HeartrateSetting.SmoothValue))
+        {
+            currentValue = (float)Interpolation.DampContinuously(currentValue, targetValue, GetSettingValue<int>(HeartrateSetting.SmoothValueLength) / 2d, 50d);
         }
         else
         {
-            currentHeartrate = targetHeartrate;
+            currentValue = targetValue;
+        }
+    }
+
+    [ModuleUpdate(ModuleUpdateMode.Custom)]
+    private void updateAverage()
+    {
+        if (!values.Any()) return;
+
+        var averageLength = TimeSpan.FromMilliseconds(GetSettingValue<int>(HeartrateSetting.AveragePeriod));
+
+        lock (valuesLock)
+        {
+            var valuesToRemove = values.Where(pair => pair.Key + averageLength <= DateTimeOffset.Now);
+            valuesToRemove.ForEach(pair => values.Remove(pair.Key));
+
+            targetAverage = (int)MathF.Round(values.Values.Sum() / (float)values.Count);
+        }
+
+        if (GetSettingValue<bool>(HeartrateSetting.SmoothAverage))
+        {
+            currentAverage = (float)Interpolation.DampContinuously(currentAverage, targetAverage, GetSettingValue<int>(HeartrateSetting.SmoothAverageLength) / 2d, 50d);
+        }
+        else
+        {
+            currentAverage = targetAverage;
         }
     }
 
@@ -131,15 +236,17 @@ public abstract class HeartrateModule<T> : AvatarModule where T : HeartrateProvi
 
         if (isReceiving)
         {
-            var normalisedHeartRate = Map(currentHeartrate, GetSettingValue<int>(HeartrateSetting.NormalisedLowerbound), GetSettingValue<int>(HeartrateSetting.NormalisedUpperbound), 0, 1);
+            var normalisedHeartRate = Map(currentValue, GetSettingValue<int>(HeartrateSetting.NormalisedLowerbound), GetSettingValue<int>(HeartrateSetting.NormalisedUpperbound), 0, 1);
 
             SendParameter(HeartrateParameter.Normalised, normalisedHeartRate);
-            SendParameter(HeartrateParameter.Value, (int)Math.Round(currentHeartrate));
+            SendParameter(HeartrateParameter.Value, (int)MathF.Round(currentValue));
+            SendParameter(HeartrateParameter.Average, (int)MathF.Round(currentAverage));
         }
         else
         {
             SendParameter(HeartrateParameter.Normalised, 0f);
             SendParameter(HeartrateParameter.Value, 0);
+            SendParameter(HeartrateParameter.Average, 0);
         }
     }
 
@@ -150,7 +257,7 @@ public abstract class HeartrateModule<T> : AvatarModule where T : HeartrateProvi
 
         if (isReceiving)
         {
-            var individualValues = toDigitArray((int)Math.Round(currentHeartrate), 3);
+            var individualValues = toDigitArray((int)Math.Round(currentValue), 3);
 
             SendParameter(LegacyHeartrateParameter.Units, individualValues[2] / 10f);
             SendParameter(LegacyHeartrateParameter.Tens, individualValues[1] / 10f);
@@ -168,17 +275,23 @@ public abstract class HeartrateModule<T> : AvatarModule where T : HeartrateProvi
 
     private enum HeartrateSetting
     {
-        Smoothing,
-        SmoothingLength,
+        SmoothValue,
+        SmoothValueLength,
+        AveragePeriod,
+        SmoothAverage,
+        SmoothAverageLength,
         NormalisedLowerbound,
-        NormalisedUpperbound
+        NormalisedUpperbound,
+        BeatMode
     }
 
     private enum HeartrateParameter
     {
         Connected,
         Normalised,
-        Value
+        Value,
+        Average,
+        Beat
     }
 
     private enum LegacyHeartrateParameter
