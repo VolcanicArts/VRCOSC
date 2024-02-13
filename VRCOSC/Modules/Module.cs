2c94f70456b56b6ee6a7739d31549c7c17b7d820
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-02-13 19:17:54 +0000

    Add module listing test

---
+++ VRCOSC/Modules/Module.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Windows;
+using System.Windows.Input;
+
+namespace VRCOSC.Modules;
+
+public class Module
+{
+    public string DisplayName { get; }
+
+    public Module(string displayName)
+    {
+        DisplayName = displayName;
+    }
+
+    #region UI
+
+    public ICommand UISettingsButton => new RelayCommand(_ => OnSettingsButtonClick());
+
+    private void OnSettingsButtonClick()
+    {
+        MessageBox.Show("WOOOOOOOO");
+    }
+
+    #endregion
+}
---
+++ VRCOSC/Pages/Modules/ModulesPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+namespace VRCOSC.Pages.Modules;
+
+public partial class ModulesPage
+{
+    private readonly ModuleViewModel moduleViewModel = new();
+
+    public ModulesPage()
+    {
+        InitializeComponent();
+
+        StackPanel.DataContext = moduleViewModel;
+    }
+}
