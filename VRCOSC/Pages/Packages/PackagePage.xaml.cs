0c457d0b82ae40d96f7bba85d8019d96c2a65b4c
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-02-15 02:57:20 +0000

    Implement search textbox

--- VRCOSC/Pages/Packages/PackagePage.xaml.cs
+++ VRCOSC/Pages/Packages/PackagePage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
+using System;
+using System.Linq;
 using System.Windows;
 using VRCOSC.Packages;
 
@@ -17,15 +19,36 @@ public partial class PackagePage
         PackageGrid.DataContext = packageManager;
         SizeChanged += OnSizeChanged;
 
+        SearchTextBox.TextChanged += (_, _) =>
+        {
+            var text = SearchTextBox.Text;
+            filterDataGrid(text);
+        };
+
+        filterDataGrid(string.Empty);
+
         AppManager.GetInstance().RegisterPage(PageLookup.Packages, this);
     }
 
-    private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+    private int itemCount;
+
+    private void OnSizeChanged(object sender, SizeChangedEventArgs e) => evaluateContentHeight();
+
+    private void evaluateContentHeight()
     {
         var packageManager = PackageManager.GetInstance();
 
-        var contentHeight = packageManager.Sources.Count * 50;
+        var contentHeight = itemCount * 50;
         var targetHeight = GridContainer.ActualHeight - 55;
         packageManager.PackageScrollViewerHeight = contentHeight >= targetHeight ? targetHeight : double.NaN;
+    }
+
+    private void filterDataGrid(string filterText)
+    {
+        var filteredItems = string.IsNullOrEmpty(filterText) ? PackageManager.GetInstance().Sources.ToList() : PackageManager.GetInstance().Sources.Where(item => item.DisplayName.Contains(filterText, StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+        itemCount = filteredItems.Count;
+        PackageGrid.ItemsSource = filteredItems.Any() ? filteredItems : PackageManager.GetInstance().Sources;
+        evaluateContentHeight();
     }
 }
