3e6536bf7fb233cef3952dc3af6381d8331d1f4c
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-02-15 01:27:25 +0000

    Create basic loading overlay

---
+++ VRCOSC/LoadingOverlay.xaml.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Windows.Controls;
+
+namespace VRCOSC;
+
+public partial class LoadingOverlay : Page
+{
+    public LoadingOverlay()
+    {
+        InitializeComponent();
+    }
+}
+
