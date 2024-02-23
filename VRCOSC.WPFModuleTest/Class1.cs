63da1a77c22711bb298e639d9753d23e8ee11a2f
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-02-23 17:44:56 +0000

    Adjust margins and add 2nd test module

--- VRCOSC.WPFModuleTest/Class1.cs
+++ VRCOSC.WPFModuleTest/Class1.cs
@@ -30,3 +30,32 @@ public class TestModule : Module
         StringSetting
     }
 }
+
+[ModuleTitle("Test Module 2")]
+[ModuleDescription("This is my 2nd test module")]
+[ModuleType(ModuleType.Generic)]
+public class TestModule2 : Module
+{
+    public override void OnPreLoad()
+    {
+        CreateToggle(TestModuleSetting.TestSetting, "Test Setting", "This is a test setting", false);
+        CreateToggle(TestModuleSetting.TestSetting2, "Another Test Setting", "This is another test setting to test the settings", true);
+        CreateToggle(TestModuleSetting.TestSetting3, "Lone Wolf", "This is a setting with no group", false);
+        CreateTextBox(TestModuleSetting.StringSetting, "String Setting", "This is a string setting", "Woooo");
+
+        CreateGroup("Best Friends", TestModuleSetting.TestSetting, TestModuleSetting.TestSetting2);
+    }
+
+    public override Task<bool> OnModuleStart()
+    {
+        return Task.FromResult(true);
+    }
+
+    private enum TestModuleSetting
+    {
+        TestSetting,
+        TestSetting2,
+        TestSetting3,
+        StringSetting
+    }
+}
