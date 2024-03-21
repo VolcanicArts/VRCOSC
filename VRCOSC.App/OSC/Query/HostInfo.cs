23646e2e555955d7a68f4558223d86f31d09816c
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-03-21 18:36:49 +0000

    Provide EXTENSIONS HOST_INFO field

--- VRCOSC.App/OSC/Query/HostInfo.cs
+++ VRCOSC.App/OSC/Query/HostInfo.cs
@@ -12,16 +12,19 @@ namespace VRCOSC.App.OSC.Query;
 public class HostInfo
 {
     [JsonProperty("NAME")]
-    public string Name = "VRCOSC";
+    public string? Name;
 
     [JsonProperty("OSC_IP")]
-    public string OSCIP = IPAddress.Loopback.ToString();
+    public string? OSCIP;
 
     [JsonProperty("OSC_PORT")]
-    public int OSCPort;
+    public int? OSCPort;
 
     [JsonProperty("OSC_TRANSPORT")]
-    public string OSCTransport = "UDP";
+    public string? OSCTransport;
+
+    [JsonProperty("EXTENSIONS")]
+    public HostInfoExtensions? Extensions;
 
     [JsonConstructor]
     public HostInfo()
@@ -30,6 +33,49 @@ public class HostInfo
 
     public HostInfo(int oscPort)
     {
+        Name = "VRCOSC";
+        OSCIP = IPAddress.Loopback.ToString();
         OSCPort = oscPort;
+        OSCTransport = "UDP";
+        Extensions = new HostInfoExtensions();
     }
+}
+
+public class HostInfoExtensions
+{
+    [JsonProperty("ACCESS")]
+    public bool Access;
+
+    [JsonProperty("VALUE")]
+    public bool Value;
+
+    [JsonProperty("RANGE")]
+    public bool Range;
+
+    [JsonProperty("Description")]
+    public bool Description;
+
+    [JsonProperty("TAGS")]
+    public bool Tags;
+
+    [JsonProperty("EXTENDED_TYPE")]
+    public bool ExtendedType;
+
+    [JsonProperty("UNIT")]
+    public bool Unit;
+
+    [JsonProperty("CRITICAL")]
+    public bool Critical;
+
+    [JsonProperty("CLIPMODE")]
+    public bool ClipMode;
+
+    [JsonProperty("OVERLOADS")]
+    public bool Overloads;
+
+    [JsonProperty("LISTEN")]
+    public bool Listen;
+
+    [JsonProperty("PATH_CHANGED")]
+    public bool PathChanged;
 }
