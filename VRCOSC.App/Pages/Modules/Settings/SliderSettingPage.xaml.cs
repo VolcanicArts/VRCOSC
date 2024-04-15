854346f3fe6db28b26eacb3bdbbdd5be5c19ee02
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-04-15 01:27:27 +0100

    Change slider to update attribute slower to reduce serialisation calls

--- VRCOSC.App/Pages/Modules/Settings/SliderSettingPage.xaml.cs
+++ VRCOSC.App/Pages/Modules/Settings/SliderSettingPage.xaml.cs
@@ -1,19 +1,28 @@
 ﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
-using System.Globalization;
+using System.Windows.Input;
 using VRCOSC.App.SDK.Modules.Attributes.Settings;
+using VRCOSC.App.Utils;
 
 namespace VRCOSC.App.Pages.Modules.Settings;
 
 public partial class SliderSettingPage
 {
+    public SliderModuleSetting SliderModuleSetting { get; }
+    public Observable<float> SliderValue { get; } = new();
+
     public SliderSettingPage(SliderModuleSetting sliderModuleSetting)
     {
         InitializeComponent();
 
-        DataContext = sliderModuleSetting;
+        SliderValue.Value = sliderModuleSetting.Attribute.Value;
+        SliderModuleSetting = sliderModuleSetting;
+        DataContext = this;
     }
 
-    private static int getDecimalPlaces(float value) => value.ToString(CultureInfo.InvariantCulture).Split('.')[1].Length;
+    private void Slider_LostMouseCapture(object sender, MouseEventArgs e)
+    {
+        SliderModuleSetting.Attribute.Value = SliderValue.Value;
+    }
 }
