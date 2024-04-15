a8122537a2d4811d4a995e29c54fa490a3211146
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-04-15 19:28:31 +0100

    Only show required decimal places for slider value

--- VRCOSC.App/Pages/Modules/Settings/SliderSettingPage.xaml.cs
+++ VRCOSC.App/Pages/Modules/Settings/SliderSettingPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 using VRCOSC.App.SDK.Modules.Attributes.Settings;
 using VRCOSC.App.Utils;
@@ -19,10 +22,31 @@ public partial class SliderSettingPage
         SliderValue.Value = sliderModuleSetting.Attribute.Value;
         SliderModuleSetting = sliderModuleSetting;
         DataContext = this;
+
+        var binding = new Binding("SliderValue.Value")
+        {
+            Mode = BindingMode.OneWay,
+            StringFormat = sliderModuleSetting.ValueType == typeof(int) ? "F0" : $"F{countDecimalPlaces(sliderModuleSetting.TickFrequency)}"
+        };
+
+        SliderValueText.SetBinding(TextBlock.TextProperty, binding);
     }
 
     private void Slider_LostMouseCapture(object sender, MouseEventArgs e)
     {
         SliderModuleSetting.Attribute.Value = SliderValue.Value;
+    }
+
+    private static int countDecimalPlaces(float number)
+    {
+        var numberString = number.ToString(CultureInfo.InvariantCulture);
+        var decimalPointIndex = numberString.IndexOf('.');
+
+        if (decimalPointIndex == -1)
+        {
+            return 0;
+        }
+
+        return numberString.Length - decimalPointIndex - 1;
     }
 }
