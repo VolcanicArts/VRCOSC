dab117f33e6ea26ec4858f542cadbb942d7da4bb
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-04-22 16:33:36 +0100

    Convert all checkboxes to VRCOSCCheckBox

---
+++ VRCOSC.App/UI/VRCOSCCheckBox.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Windows;
+using System.Windows.Controls;
+
+// ReSharper disable InconsistentNaming
+
+namespace VRCOSC.App.UI;
+
+public class VRCOSCCheckBox : CheckBox
+{
+    public static readonly DependencyProperty IconMarginProperty =
+        DependencyProperty.Register(nameof(IconMargin), typeof(Thickness), typeof(VRCOSCCheckBox), new PropertyMetadata(new Thickness(5f)));
+
+    public Thickness IconMargin
+    {
+        get => (Thickness)GetValue(IconMarginProperty);
+        set => SetValue(IconMarginProperty, value);
+    }
+
+    public static readonly DependencyProperty CornerRadiusProperty =
+        DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(VRCOSCCheckBox), new PropertyMetadata(new CornerRadius(5f)));
+
+    public CornerRadius CornerRadius
+    {
+        get => (CornerRadius)GetValue(CornerRadiusProperty);
+        set => SetValue(CornerRadiusProperty, value);
+    }
+}
