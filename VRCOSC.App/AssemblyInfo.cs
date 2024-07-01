49cbd8c8e74b97cecab663c9fb9011cde51d6223
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-07-01 23:13:59 +0100

    Update AssemblyInfos

--- VRCOSC.App/AssemblyInfo.cs
+++ VRCOSC.App/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Windows;
 
 [assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    ResourceDictionaryLocation.SourceAssembly, //where theme specific resource dictionaries are located
     //(used if a resource is not found in the page,
     // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
