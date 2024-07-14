f812e00339bfe184254f1e08827287f6c5b6c330
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-07-14 15:19:43 +0100

    Guard against incorrect Whisper model paths

--- VRCOSC.App/Audio/Whisper/AudioProcessor.cs
+++ VRCOSC.App/Audio/Whisper/AudioProcessor.cs
@@ -1,47 +1,58 @@
 ﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using NAudio.CoreAudioApi;
 using VRCOSC.App.Settings;
+using VRCOSC.App.Utils;
 using Whisper.net;
 
 namespace VRCOSC.App.Audio.Whisper;
 
 internal class AudioProcessor
 {
-    private readonly AudioCapture audioCapture;
-    private readonly WhisperProcessor whisper;
+    private readonly AudioCapture? audioCapture;
+    private readonly WhisperProcessor? whisper;
 
     public AudioProcessor(MMDevice device)
     {
         var modelFilePath = SettingsManager.GetInstance().GetValue<string>(VRCOSCSetting.Whisper_ModelPath);
 
-        var builder = WhisperFactory.FromPath(modelFilePath);
+        try
+        {
+            var builder = WhisperFactory.FromPath(modelFilePath);
 
-        whisper = builder.CreateBuilder()
-                         .WithProbabilities()
-                         .WithNoContext()
-                         .Build();
+            whisper = builder.CreateBuilder()
+                             .WithProbabilities()
+                             .WithNoContext()
+                             .Build();
 
-        audioCapture = new AudioCapture(device);
+            audioCapture = new AudioCapture(device);
+        }
+        catch (Exception)
+        {
+            ExceptionHandler.Handle("The model path for Whisper is incorrect");
+        }
     }
 
     public void Start()
     {
-        audioCapture.ClearBuffer();
-        audioCapture.StartCapture();
+        audioCapture?.ClearBuffer();
+        audioCapture?.StartCapture();
     }
 
     public void Stop()
     {
-        audioCapture.StopCapture();
+        audioCapture?.StopCapture();
     }
 
     public async Task<SpeechResult?> GetResult()
     {
+        if (audioCapture is null) return new SpeechResult(string.Empty, 0f);
+
         if (!audioCapture.IsCapturing) return null;
 
         var data = audioCapture.GetBufferedData();
@@ -55,7 +66,7 @@ internal class AudioProcessor
     {
         var segmentData = new List<SegmentData>();
 
-        await foreach (var result in whisper.ProcessAsync(data))
+        await foreach (var result in whisper!.ProcessAsync(data))
         {
             segmentData.Add(result);
         }
@@ -70,6 +81,6 @@ internal class AudioProcessor
 
     public void ClearBuffer()
     {
-        audioCapture.ClearBuffer();
+        audioCapture?.ClearBuffer();
     }
 }
