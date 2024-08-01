f254dce98c8bb1b69c62f20b2235449fdf4db060
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-08-02 00:30:31 +0100

    Add missing file headers

--- VRCOSC.App/UI/Windows/Modules/ModulePrefabsWindow.xaml.cs
+++ VRCOSC.App/UI/Windows/Modules/ModulePrefabsWindow.xaml.cs
@@ -1,3 +1,6 @@
+// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
 using System.Collections.Generic;
 using System.Windows;
 using VRCOSC.App.SDK.Modules;
