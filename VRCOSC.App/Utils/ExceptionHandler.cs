be403cb1c45d137f408c7f7ad42a9d1bdfd2887e
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-08-09 20:07:05 +0100

    Update ExceptionHandler formatting

--- VRCOSC.App/Utils/ExceptionHandler.cs
+++ VRCOSC.App/Utils/ExceptionHandler.cs
@@ -2,7 +2,6 @@
 // See the LICENSE file in the repository root for full license text.
 
 using System;
-using System.Diagnostics;
 using System.Text;
 using System.Windows;
 
@@ -22,16 +21,21 @@ public static class ExceptionHandler
 
         var sb = new StringBuilder();
 
+        sb.AppendLine("-------------------------------------------------------------");
+        sb.AppendLine($"Version: {AppManager.Version}");
+        sb.AppendLine();
+
         if (!string.IsNullOrEmpty(message))
         {
             sb.AppendLine(message);
             sb.AppendLine();
         }
 
-        sb.AppendLine("Please report this on the Discord server or ask for help if you're developing a module");
+        sb.AppendLine("Please report this on the Discord server");
         sb.AppendLine();
-        sb.AppendLine("Press OK to join Discord server and report the error");
+        sb.AppendLine("Press OK to join the Discord server");
         sb.AppendLine("Press Cancel to ignore the error");
+        sb.AppendLine("-------------------------------------------------------------");
         sb.AppendLine();
 
         var proxyException = e;
@@ -49,12 +53,7 @@ public static class ExceptionHandler
 
         if (result == MessageBoxResult.OK)
         {
-            Process.Start(new ProcessStartInfo
-            {
-                FileName = VRCOSCLinks.DISCORD_INVITE,
-                UseShellExecute = true,
-                WorkingDirectory = Environment.CurrentDirectory
-            });
+            VRCOSCLinks.DISCORD_INVITE.OpenExternally();
         }
 
         isWindowShowing = false;
