3b703b73a3c23b572180269df4b6e828ba831d66
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-08-27 19:11:32 +0100

    Only list modules that have states

--- VRCOSC.App/UI/Windows/ChatBox/ChatBoxClipEditWindow.xaml.cs
+++ VRCOSC.App/UI/Windows/ChatBox/ChatBoxClipEditWindow.xaml.cs
@@ -40,7 +40,7 @@ public partial class ChatBoxClipEditWindow
         DataContext = referenceClip;
         ReferenceClip = referenceClip;
 
-        ModulesList.ItemsSource = ModuleManager.GetInstance().Modules.Values.SelectMany(moduleList => moduleList).Where(module => ChatBoxManager.GetInstance().DoesModuleHaveStates(module.FullID) || ChatBoxManager.GetInstance().DoesModuleHaveEvents(module.FullID)).OrderBy(module => module.IsRemote).ThenBy(module => module.PackageID).ThenBy(module => module.Title);
+        ModulesList.ItemsSource = ModuleManager.GetInstance().Modules.Values.SelectMany(moduleList => moduleList).Where(module => ChatBoxManager.GetInstance().DoesModuleHaveStates(module.FullID)).OrderBy(module => module.IsRemote).ThenBy(module => module.PackageID).ThenBy(module => module.Title);
         ShowRelevantModulesCheckBox.DataContext = this;
 
         ReferenceClip.Name.Subscribe(newName => Title = $"Editing {newName} Clip", true);
