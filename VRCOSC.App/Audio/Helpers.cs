c35ec42628d9fc5aa6c86fb9daf6d67abe4e4a48
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-10-30 18:51:25 +0000

    Remove unused WaveHelper class

--- VRCOSC.App/Audio/Helpers.cs
+++ VRCOSC.App/Audio/Helpers.cs
@@ -1,4 +1,4 @@
-// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
 using System;
@@ -41,70 +41,6 @@ public static class AudioDeviceHelper
         {
             ExceptionHandler.Handle(e);
             return null;
-        }
-    }
-}
-
-public static class WaveHelper
-{
-    #region Band Pass Filter
-
-    public static void ApplyBandPassFilter(float[] waveStream, float sampleRate, float lowCutoffFrequency, float highCutoffFrequency)
-    {
-        if (waveStream.Length == 0)
-        {
-            throw new ArgumentException("The waveStream array must not be empty");
-        }
-
-        ApplyHighPassFilter(waveStream, lowCutoffFrequency, sampleRate);
-        ApplyLowPassFilter(waveStream, highCutoffFrequency, sampleRate);
-    }
-
-    public static void ApplyHighPassFilter(float[] waveStream, float sampleRate, float cutoffFrequency)
-    {
-        var rc = 1.0f / (cutoffFrequency * 2 * MathF.PI);
-        var dt = 1.0f / sampleRate;
-        var alpha = rc / (rc + dt);
-
-        var previousInput = waveStream[0];
-        var previousOutput = waveStream[0];
-
-        for (var i = 1; i < waveStream.Length; i++)
-        {
-            var currentInput = waveStream[i];
-            waveStream[i] = alpha * (previousOutput + currentInput - previousInput);
-            previousInput = currentInput;
-            previousOutput = waveStream[i];
-        }
-    }
-
-    public static void ApplyLowPassFilter(float[] waveStream, float sampleRate, float cutoffFrequency)
-    {
-        var rc = 1.0f / (cutoffFrequency * 2 * MathF.PI);
-        var dt = 1.0f / sampleRate;
-        var alpha = dt / (rc + dt);
-
-        var previous = waveStream[0];
-
-        for (var i = 1; i < waveStream.Length; i++)
-        {
-            waveStream[i] = previous + (alpha * (waveStream[i] - previous));
-            previous = waveStream[i];
-        }
-    }
-
-    #endregion
-
-    public static void ApplyCompression(float[] buffer, float threshold, float ratio)
-    {
-        var linearThreshold = (float)Math.Pow(10, threshold / 20.0f); // Convert dB to linear scale
-
-        for (var i = 0; i < buffer.Length; i++)
-        {
-            if (buffer[i] > linearThreshold)
-            {
-                buffer[i] = linearThreshold + (buffer[i] - linearThreshold) / ratio;
-            }
         }
     }
 }
