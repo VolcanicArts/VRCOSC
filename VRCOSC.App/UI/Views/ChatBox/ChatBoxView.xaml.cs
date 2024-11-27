9a8a621cb4db2a74100e77090601e32d7cbdd8c7
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-11-27 20:03:13 +0000

    Fix clearing the timeline not deselecting the selected clip

--- VRCOSC.App/UI/Views/ChatBox/ChatBoxView.xaml.cs
+++ VRCOSC.App/UI/Views/ChatBox/ChatBoxView.xaml.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
 using System;
@@ -423,6 +423,7 @@ public partial class ChatBoxView
 
         // Clearing the clips doesn't fire the collection changed event. This fixes it
         ChatBoxManager.GetInstance().Timeline.Clips.RemoveIf(_ => true);
+        SelectedClip = null;
     }
 
     private void showRightClickMenu(int? layer, Clip? clip)
