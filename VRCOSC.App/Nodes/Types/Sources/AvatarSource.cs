0a318bf43611dc9b86b1bb2933f62550c26359ef
VolcanicArts <VolcanicArtsBP@Gmail.com>
2025-05-28 19:21:52 +0100

    Add avatar source node

---
+++ VRCOSC.App/Nodes/Types/Sources/AvatarSource.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using VRCOSC.App.SDK.Nodes;
+
+namespace VRCOSC.App.Nodes.Types.Sources;
+
+[Node("Avatar Source", "Sources")]
+public class AvatarSourceNode : Node
+{
+    public ValueOutput<string> AvatarId = new();
+    public ValueOutput<string> Name = new();
+
+    protected override void Process(PulseContext c)
+    {
+        var avatar = c.FindCurrentAvatar();
+        if (avatar is null) return;
+
+        AvatarId.Write(avatar.Id, c);
+        Name.Write(avatar.Name, c);
+    }
+}
