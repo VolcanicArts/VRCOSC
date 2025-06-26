2f50d687d7221506e33b8b878c6e80c806db710a
VolcanicArts <VolcanicArtsBP@Gmail.com>
2025-06-26 21:56:41 +0100

    Add support for dictionaries and improve enumerable nodes

--- VRCOSC.App/Nodes/Types/Collections/Create.cs
+++ VRCOSC.App/Nodes/Types/Collections/Create.cs
@@ -4,18 +4,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VRCOSC.App.Utils;
 
 namespace VRCOSC.App.Nodes.Types.Collections;
 
-[Node("Construct List", "Collections")]
-public class ConstructListNode<T> : Node
+[Node("Create List", "Collections")]
+public class CreateListNode<T> : Node
 {
-    public ValueInputList<T> Items = new();
-    public ValueOutput<List<T>> List = new();
+    public ValueInputList<T> Inputs = new();
+    public ValueOutput<List<T>> Output = new();
+    public ValueOutput<int> InputCount = new("Input Count");
 
     protected override Task Process(PulseContext c)
     {
-        List.Write(Items.Read(c).ToList(), c);
+        var list = Inputs.Read(c).ToList();
+        Output.Write(list, c);
+        InputCount.Write(list.Count, c);
+        return Task.CompletedTask;
+    }
+}
+
+[Node("Create Dictionary", "Collections")]
+public class CreateDictionaryNode<TKey, TValue> : Node where TKey : notnull
+{
+    public ValueInputList<KeyValuePair<TKey, TValue>> Inputs = new();
+    public ValueOutput<Dictionary<TKey, TValue>> Output = new();
+    public ValueOutput<int> InputCount = new("Input Count");
+
+    protected override Task Process(PulseContext c)
+    {
+        var dictionary = new Dictionary<TKey, TValue>();
+        dictionary.AddRange(Inputs.Read(c).RemoveIf(pair => pair.Key is null));
+
+        Output.Write(dictionary, c);
+        InputCount.Write(dictionary.Count, c);
+        return Task.CompletedTask;
+    }
+}
+
+[Node("Create KeyValuePair", "Collections")]
+public class CreateKeyValuePairNode<TKey, TValue> : Node where TKey : notnull
+{
+    public ValueInput<TKey> Key = new();
+    public ValueInput<TValue> Value = new();
+    public ValueOutput<KeyValuePair<TKey, TValue>> Output = new();
+
+    protected override Task Process(PulseContext c)
+    {
+        Output.Write(new KeyValuePair<TKey, TValue>(Key.Read(c), Value.Read(c)), c);
         return Task.CompletedTask;
     }
 }
---
+++ VRCOSC.App/Nodes/Types/Collections/Enumerable.cs
@@ -0,0 +1,146 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VRCOSC.App.Nodes.Types.Collections;
+
+[Node("Enumerable Count", "Collections")]
+public class EnumerableCountNode<T> : Node
+{
+    public ValueInput<IEnumerable<T>> Enumerable = new();
+    public ValueOutput<int> Count = new();
+
+    protected override Task Process(PulseContext c)
+    {
+        var enumerable = Enumerable.Read(c);
+        if (enumerable is null) return Task.CompletedTask;
+
+        Count.Write(enumerable.Count(), c);
+        return Task.CompletedTask;
+    }
+}
+
+[Node("Enumerable Element At", "Collections")]
+public sealed class EnumerableElementAtNode<T> : Node
+{
+    public ValueInput<IEnumerable<T>> Enumerable = new();
+    public ValueInput<int> Index = new();
+    public ValueOutput<T> Element = new();
+
+    protected override Task Process(PulseContext c)
+    {
+        var enumerable = Enumerable.Read(c);
+        if (enumerable is null) return Task.CompletedTask;
+
+        var index = Index.Read(c);
+        if (index < 0 || index >= enumerable.Count()) return Task.CompletedTask;
+
+        Element.Write(enumerable.ElementAt(index), c);
+        return Task.CompletedTask;
+    }
+}
+
+[Node("Enumerable Insert Element", "Collections")]
+public class EnumerableElementInsertNode<T> : Node, IFlowInput
+{
+    public FlowContinuation Next = new("Next");
+
+    public ValueInput<IEnumerable<T>> Enumerable = new();
+    public ValueInput<int> Index = new();
+    public ValueInput<T> Element = new();
+    public ValueOutput<IEnumerable<T>> Result = new();
+
+    protected override Task Process(PulseContext c)
+    {
+        var enumerable = Enumerable.Read(c);
+        if (enumerable is null) return Task.CompletedTask;
+
+        var index = Index.Read(c);
+        var element = Element.Read(c);
+
+        var list = enumerable.ToList();
+        list.Insert(index, element);
+        Result.Write(list, c);
+
+        Next.Execute(c);
+        return Task.CompletedTask;
+    }
+}
+
+[Node("Enumerable Add Element", "Collections")]
+public sealed class EnumerableElementAddNode<T> : Node, IFlowInput
+{
+    public FlowContinuation Next = new("Next");
+
+    public ValueInput<IEnumerable<T>> Enumerable = new();
+    public ValueInput<T> Element = new();
+    public ValueOutput<IEnumerable<T>> Result = new();
+
+    protected override Task Process(PulseContext c)
+    {
+        var enumerable = Enumerable.Read(c);
+        if (enumerable is null) return Task.CompletedTask;
+
+        var element = Element.Read(c);
+
+        var list = enumerable.ToList();
+        list.Add(element);
+        Result.Write(list, c);
+
+        Next.Execute(c);
+        return Task.CompletedTask;
+    }
+}
+
+[Node("Enumerable Remove Element", "Collections")]
+public sealed class EnumerableElementRemoveNode<T> : Node, IFlowInput
+{
+    public FlowContinuation Next = new("Next");
+
+    public ValueInput<IEnumerable<T>> Enumerable = new();
+    public ValueInput<T> Element = new();
+    public ValueOutput<IEnumerable<T>> Result = new();
+
+    protected override Task Process(PulseContext c)
+    {
+        var enumerable = Enumerable.Read(c);
+        if (enumerable is null) return Task.CompletedTask;
+
+        var element = Element.Read(c);
+
+        var list = enumerable.ToList();
+        list.Remove(element);
+        Result.Write(list, c);
+
+        Next.Execute(c);
+        return Task.CompletedTask;
+    }
+}
+
+[Node("Enumerable Remove Index", "Collections")]
+public sealed class EnumerableIndexRemoveNode<T> : Node, IFlowInput
+{
+    public FlowContinuation Next = new("Next");
+
+    public ValueInput<IEnumerable<T>> Enumerable = new();
+    public ValueInput<int> Index = new();
+    public ValueOutput<IEnumerable<T>> Result = new();
+
+    protected override Task Process(PulseContext c)
+    {
+        var enumerable = Enumerable.Read(c);
+        if (enumerable is null) return Task.CompletedTask;
+
+        var index = Index.Read(c);
+
+        var list = enumerable.ToList();
+        list.RemoveAt(index);
+        Result.Write(list, c);
+
+        Next.Execute(c);
+        return Task.CompletedTask;
+    }
+}
