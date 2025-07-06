2f2262ca6e4ff8789e8a3b9735b314bfce9db9a3
VolcanicArts <VolcanicArtsBP@Gmail.com>
2025-07-06 13:27:07 +0100

    Improve AvatarSource node

--- VRCOSC.App/Nodes/Types/Sources/AvatarSource.cs
+++ VRCOSC.App/Nodes/Types/Sources/AvatarSource.cs
@@ -12,22 +12,11 @@ public sealed class AvatarSourceNode : Node, INodeEventHandler
     public ValueOutput<string> AvatarId = new("Id");
     public ValueOutput<string> Name = new();
 
-    protected override Task Process(PulseContext c)
+    protected override async Task Process(PulseContext c)
     {
-        var avatar = c.FindCurrentAvatar();
-
-        if (avatar is null)
-        {
-            AvatarId.Write(null!, c);
-            Name.Write(string.Empty, c);
-        }
-        else
-        {
-            AvatarId.Write(avatar.Id, c);
-            Name.Write(avatar.Name, c);
-        }
-
-        return Task.CompletedTask;
+        var avatar = await c.GetCurrentAvatar();
+        AvatarId.Write(avatar?.Id ?? null!, c);
+        Name.Write(avatar?.Name ?? string.Empty, c);
     }
 
     public bool HandleAvatarChange(PulseContext c, AvatarConfig? config) => true;
