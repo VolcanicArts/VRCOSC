e790d00f46c6590f7142b70ee06903f29f072dd1
VolcanicArts <VolcanicArtsBP@Gmail.com>
2025-10-15 17:55:35 +0100

    Remove unneeded node attributes

--- VRCOSC.App/Nodes/Types/Math/Constants/Constants.cs
+++ VRCOSC.App/Nodes/Types/Math/Constants/Constants.cs
@@ -6,21 +6,18 @@ using System;
 namespace VRCOSC.App.Nodes.Types.Math.Constants;
 
 [Node("E", "Math/Constants")]
-[NodeCollapsed]
 public sealed class EConstantNode : ConstantNode<float>
 {
     protected override float GetValue() => MathF.E;
 }
 
 [Node("Pi", "Math/Constants")]
-[NodeCollapsed]
 public sealed class PiConstantNode : ConstantNode<float>
 {
     protected override float GetValue() => MathF.PI;
 }
 
 [Node("Tau", "Math/Constants")]
-[NodeCollapsed]
 public sealed class TauConstantNode : ConstantNode<float>
 {
     protected override float GetValue() => MathF.Tau;
