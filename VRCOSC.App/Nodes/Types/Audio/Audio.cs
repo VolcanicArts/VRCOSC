195b8430a7612b977f35e4e8aa518c7eb6f05e82
VolcanicArts <VolcanicArtsBP@Gmail.com>
2025-10-18 16:47:40 +0100

    Rework audio node handling

--- VRCOSC.App/Nodes/Types/Audio/Audio.cs
+++ VRCOSC.App/Nodes/Types/Audio/Audio.cs
@@ -8,41 +8,110 @@ using SoundFlow.Providers;
 
 namespace VRCOSC.App.Nodes.Types.Audio;
 
-[Node("Audio Play Once", "Audio")]
-public sealed class AudioPlayOnceNode : Node, IFlowInput
+[Node("Create Track", "Audio")]
+public sealed class AudioTrackCreateNode : Node
 {
-    public FlowContinuation OnFinished = new("On Finished");
-    public FlowContinuation OnFailed = new("On Failed");
-
     public ValueInput<string> FilePath = new("File Path");
     public ValueInput<float> Volume = new("Volume", 1f);
+    public ValueInput<float> Speed = new("Speed", 1f);
 
-    protected override async Task Process(PulseContext c)
+    public ValueOutput<SoundPlayer> Track = new();
+
+    protected override Task Process(PulseContext c)
     {
         var filePath = FilePath.Read(c);
 
         if (string.IsNullOrWhiteSpace(filePath) || !File.Exists(filePath))
+            return Task.CompletedTask;
+
+        var player = new SoundPlayer(new StreamDataProvider(File.OpenRead(filePath)))
+        {
+            Volume = Volume.Read(c),
+            PlaybackSpeed = Speed.Read(c)
+        };
+
+        Mixer.Master.AddComponent(player);
+
+        Track.Write(player, c);
+        return Task.CompletedTask;
+    }
+}
+
+[Node("Play Track", "Audio")]
+public sealed class AudioTrackPlayNode : Node, IFlowInput
+{
+    public FlowContinuation OnFinished = new("On Finished");
+    public FlowContinuation OnFailed = new("On Failed");
+
+    public ValueInput<SoundPlayer> Track = new();
+
+    protected override async Task Process(PulseContext c)
+    {
+        var player = Track.Read(c);
+
+        if (player is null)
         {
             await OnFailed.Execute(c);
             return;
         }
 
-        var player = new SoundPlayer(new StreamDataProvider(File.OpenRead(filePath)))
-        {
-            Volume = Volume.Read(c),
-        };
-
-        Mixer.Master.AddComponent(player);
         player.Play();
 
         while (player.Time < player.Duration)
         {
-            if (c.IsCancelled) break;
+            await Task.Delay(10);
         }
 
         player.Stop();
-        Mixer.Master.RemoveComponent(player);
 
         await OnFinished.Execute(c);
+    }
+}
+
+[Node("Play Track Async", "Audio")]
+public sealed class AudioTrackPlayAsyncNode : Node, IFlowInput
+{
+    public FlowContinuation OnPlaying = new("On Playing");
+    public FlowContinuation OnFailed = new("On Failed");
+
+    public ValueInput<SoundPlayer> Track = new();
+
+    protected override async Task Process(PulseContext c)
+    {
+        var player = Track.Read(c);
+
+        if (player is null)
+        {
+            await OnFailed.Execute(c);
+            return;
+        }
+
+        player.Play();
+
+        await OnPlaying.Execute(c);
+    }
+}
+
+[Node("Stop Track", "Audio")]
+public sealed class AudioTrackStopNode : Node, IFlowInput
+{
+    public FlowContinuation OnStopped = new("On Stopped");
+    public FlowContinuation OnFailed = new("On Failed");
+
+    public ValueInput<SoundPlayer> Track = new();
+
+    protected override async Task Process(PulseContext c)
+    {
+        var player = Track.Read(c);
+
+        if (player is null)
+        {
+            await OnFailed.Execute(c);
+            return;
+        }
+
+        player.Stop();
+
+        await OnStopped.Execute(c);
     }
 }
