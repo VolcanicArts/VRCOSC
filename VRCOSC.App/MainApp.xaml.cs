52840af596f944b43f0a22c1002e228f55424afb
VolcanicArts <VolcanicArtsBP@Gmail.com>
2025-10-18 19:17:35 +0100

    Add more unhandled error handling

--- VRCOSC.App/MainApp.xaml.cs
+++ VRCOSC.App/MainApp.xaml.cs
@@ -2,6 +2,7 @@
 // See the LICENSE file in the repository root for full license text.
 
 using System;
+using System.Threading.Tasks;
 using VRCOSC.App.Utils;
 
 namespace VRCOSC.App;
@@ -12,6 +13,31 @@ public partial class MainApp
     {
         InitializeComponent();
 
-        AppDomain.CurrentDomain.UnhandledException += (_, e) => ExceptionHandler.Handle((Exception)e.ExceptionObject, "An unhandled exception has occured", true);
+        Current.DispatcherUnhandledException += (s, ex) =>
+        {
+            ExceptionHandler.Handle(ex.Exception, "DispatcherUnhandledException", true);
+            ex.Handled = false;
+        };
+
+        TaskScheduler.UnobservedTaskException += (s, ex) =>
+        {
+            ExceptionHandler.Handle(ex.Exception, "UnobservedTaskException", true);
+            ex.SetObserved();
+        };
+
+        AppDomain.CurrentDomain.UnhandledException += (s, ex) =>
+        {
+            ExceptionHandler.Handle((ex.ExceptionObject as Exception)!, "An unhandled exception has occured", true);
+        };
+
+        AppDomain.CurrentDomain.FirstChanceException += (s, ex) =>
+        {
+            if (ex.Exception.Source?.Contains("PresentationCore") == true ||
+                ex.Exception.ToString().Contains("InputMethod") ||
+                ex.Exception.ToString().Contains("ITfThreadMgr"))
+            {
+                ExceptionHandler.Handle(ex.Exception, "FirstChanceException (input/TSF)");
+            }
+        };
     }
 }
