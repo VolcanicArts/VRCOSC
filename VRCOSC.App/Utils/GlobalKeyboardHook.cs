75dbe45e5447f0db8b02267bcf9baf3c0e5bdf36
VolcanicArts <VolcanicArtsBP@Gmail.com>
2025-11-11 19:09:37 +0000

    Fix latency caused by GlobalKeyboardHook in other apps

--- VRCOSC.App/Utils/GlobalKeyboardHook.cs
+++ VRCOSC.App/Utils/GlobalKeyboardHook.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Input;
 using Windows.Win32;
 using Windows.Win32.Foundation;
@@ -18,7 +18,14 @@ internal class GlobalKeyboardHook : IDisposable
     private readonly HOOKPROC hookProc;
     private HHOOK hookId;
 
-    private readonly Dictionary<Key, bool> keyStates = [];
+    private Thread? hookThread;
+    private uint hookNativeThreadId;
+    private readonly ManualResetEventSlim hookReady = new();
+    private volatile bool stopRequested;
+
+    private readonly Dictionary<Key, bool> keyStates = new();
+    private readonly Lock stateLock = new();
+
     public bool IsEnabled { get; private set; }
 
     public GlobalKeyboardHook()
@@ -27,14 +34,38 @@ internal class GlobalKeyboardHook : IDisposable
         hookId = new HHOOK(IntPtr.Zero);
     }
 
-    public bool GetKeyState(Key key) => keyStates.GetValueOrDefault(key, false);
+    public bool GetKeyState(Key key)
+    {
+        lock (stateLock)
+            return keyStates.GetValueOrDefault(key, false);
+    }
 
     public unsafe void Enable()
     {
-        if (hookId.Value != (void*)IntPtr.Zero)
-            return;
+        if (IsEnabled) return;
 
-        hookId = PInvoke.SetWindowsHookEx(WINDOWS_HOOK_ID.WH_KEYBOARD_LL, hookProc, HINSTANCE.Null, 0);
+        stopRequested = false;
+        hookReady.Reset();
+
+        hookThread = new Thread(hookThreadMain)
+        {
+            IsBackground = true,
+            Name = $"{AppManager.APP_NAME} {nameof(GlobalKeyboardHook)}",
+            Priority = ThreadPriority.Highest
+        };
+
+        try
+        {
+            hookThread.SetApartmentState(ApartmentState.STA);
+        }
+        catch
+        {
+        }
+
+        hookThread.Start();
+
+        if (!hookReady.Wait(TimeSpan.FromSeconds(2)))
+            throw new TimeoutException("Timed out waiting for global keyboard hook to initialize");
 
         if (hookId.Value == (void*)IntPtr.Zero)
             throw new Win32Exception(Marshal.GetLastWin32Error(), "Failed to set global keyboard hook");
@@ -42,34 +73,76 @@ internal class GlobalKeyboardHook : IDisposable
         IsEnabled = true;
     }
 
-    public unsafe void Disable()
+    public void Disable()
     {
-        if (hookId.Value != (void*)IntPtr.Zero)
+        if (!IsEnabled) return;
+
+        stopRequested = true;
+
+        if (hookNativeThreadId != 0)
         {
-            PInvoke.UnhookWindowsHookEx(hookId);
-            hookId = new HHOOK(IntPtr.Zero);
+            PInvoke.PostThreadMessage(hookNativeThreadId, wm_quit, new WPARAM(), new LPARAM());
         }
 
-        keyStates.Clear();
+        try
+        {
+            hookThread?.Join(TimeSpan.FromSeconds(2));
+        }
+        catch
+        {
+        }
+
+        hookThread = null;
+        hookNativeThreadId = 0;
+
+        lock (stateLock) keyStates.Clear();
+
         IsEnabled = false;
+    }
+
+    private unsafe void hookThreadMain()
+    {
+        hookNativeThreadId = PInvoke.GetCurrentThreadId();
+
+        hookId = PInvoke.SetWindowsHookEx(
+            WINDOWS_HOOK_ID.WH_KEYBOARD_LL,
+            hookProc,
+            HINSTANCE.Null,
+            0
+        );
+
+        hookReady.Set();
+
+        if (hookId.Value == (void*)IntPtr.Zero) return;
+
+        while (PInvoke.GetMessage(out var msg, HWND.Null, 0, 0))
+        {
+            if (stopRequested && msg.message == wm_quit)
+                break;
+
+            PInvoke.TranslateMessage(msg);
+            PInvoke.DispatchMessage(msg);
+        }
+
+        if (hookId.Value == (void*)IntPtr.Zero) return;
+
+        PInvoke.UnhookWindowsHookEx(hookId);
+        hookId = new HHOOK(IntPtr.Zero);
     }
 
     private LRESULT hookCallback(int nCode, WPARAM wParam, LPARAM lParam)
     {
-        Task.Run(() =>
-        {
-            if (nCode < 0) return;
+        var msg = (uint)wParam.Value;
 
-            var msg = (uint)wParam.Value;
+        if (msg is not (wm_keydown or wm_syskeydown or wm_keyup or wm_syskeyup))
+            return PInvoke.CallNextHookEx(hookId, nCode, wParam, lParam);
 
-            if (msg is not (wm_keydown or wm_syskeydown or wm_keyup or wm_syskeyup)) return;
+        var info = Marshal.PtrToStructure<KBDLLHOOKSTRUCT>(lParam.Value);
+        var key = KeyInterop.KeyFromVirtualKey((int)info.vkCode);
+        var isKeyDown = msg is wm_keydown or wm_syskeydown;
 
-            var info = Marshal.PtrToStructure<KBDLLHOOKSTRUCT>(lParam.Value);
-            var key = KeyInterop.KeyFromVirtualKey((int)info.vkCode);
-            var isKeyDown = msg is wm_keydown or wm_syskeydown;
-
+        lock (stateLock)
             keyStates[key] = isKeyDown;
-        });
 
         return PInvoke.CallNextHookEx(hookId, nCode, wParam, lParam);
     }
@@ -84,4 +157,5 @@ internal class GlobalKeyboardHook : IDisposable
     private const uint wm_keyup = 0x0101;
     private const uint wm_syskeydown = 0x0104;
     private const uint wm_syskeyup = 0x0105;
+    private const uint wm_quit = 0x0012;
 }
