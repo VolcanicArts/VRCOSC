c4bcc0058ad2bf548076daf8f48cb51ac539d467
VolcanicArts <VolcanicArtsBP@Gmail.com>
2026-04-12 14:07:55 +0100

    Implement value compute and value source nodes

--- VRCOSC.App/Nodes/Types/Time/Sources.cs
+++ VRCOSC.App/Nodes/Types/Time/Sources.cs
@@ -2,54 +2,20 @@
 // See the LICENSE file in the repository root for full license text.
 
 using System;
-using System.Threading.Tasks;
 
 namespace VRCOSC.App.Nodes.Types.Time;
 
 [Node("DateTime Now", "Date & Time")]
 [NodeCollapsed]
-public sealed class DateTimeNowSourceNode : UpdateNode<DateTime>
-{
-    public ValueOutput<DateTime> DateTime = new();
-
-    protected override Task Process(PulseContext c)
-    {
-        DateTime.Write(System.DateTime.Now, c);
-        return Task.CompletedTask;
-    }
-
-    protected override Task<DateTime> GetValue(PulseContext c) => Task.FromResult(System.DateTime.Now);
-}
+public sealed class DateTimeNowSourceNode() : ValueSourceNode<DateTime>(() => DateTime.Now);
 
 [Node("Date Today", "Date & Time")]
 [NodeCollapsed]
-public sealed class DateTimeTodaySourceNode : UpdateNode<DateTime>
-{
-    public ValueOutput<DateTime> Date = new();
-
-    protected override Task Process(PulseContext c)
-    {
-        Date.Write(DateTime.Today, c);
-        return Task.CompletedTask;
-    }
-
-    protected override Task<DateTime> GetValue(PulseContext c) => Task.FromResult(DateTime.Today);
-}
+public sealed class DateTimeTodaySourceNode() : ValueSourceNode<DateTime>(() => DateTime.Today);
 
 [Node("UTC Now", "Date & Time")]
 [NodeCollapsed]
-public sealed class DateTimeUtcNowSourceNode : UpdateNode<DateTime>
-{
-    public ValueOutput<DateTime> UTCNow = new();
-
-    protected override Task Process(PulseContext c)
-    {
-        UTCNow.Write(DateTime.UtcNow, c);
-        return Task.CompletedTask;
-    }
-
-    protected override Task<DateTime> GetValue(PulseContext c) => Task.FromResult(DateTime.UtcNow);
-}
+public sealed class DateTimeUtcNowSourceNode() : ValueSourceNode<DateTime>(() => DateTime.UtcNow);
 
 [Node("Unix Epoch", "Date & Time")]
 public sealed class DateTimeUnixEpochConstantNode() : ConstantNode<DateTime>(DateTime.UnixEpoch);
---
+++ VRCOSC.App/Nodes/Types/ValueSourceNode.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace VRCOSC.App.Nodes.Types;
+
+public abstract class ValueSourceNode<T1> : ValueComputeNode<T1>, IActiveUpdateNode
+{
+    public virtual int UpdateOffset => 0;
+    private readonly GlobalStore<T1> prevValue = new();
+
+    private readonly Func<T1> _func;
+
+    protected ValueSourceNode(Func<T1> func) => _func = func;
+
+    protected override T1 ComputeValue(PulseContext c) => prevValue.Read(c);
+
+    public Task<bool> OnUpdate(PulseContext c)
+    {
+        var value = _func();
+
+        if (EqualityComparer<T1>.Default.Equals(value, prevValue.Read(c))) return Task.FromResult(false);
+
+        prevValue.Write(value, c);
+        return Task.FromResult(true);
+    }
+}
