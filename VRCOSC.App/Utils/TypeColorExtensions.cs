c0414178c9ba4c5a3d6a7594bda3530232ee8e76
VolcanicArts <VolcanicArtsBP@Gmail.com>
2026-05-06 18:57:58 +0100

    Adjust char type colour

--- VRCOSC.App/Utils/TypeColorExtensions.cs
+++ VRCOSC.App/Utils/TypeColorExtensions.cs
@@ -23,7 +23,7 @@ public static class TypeColorExtensions
         if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>)) type = type.GenericTypeArguments[0];
 
         if (type == typeof(bool)) return fromHsl(0, 0, 0.6f);
-        if (type == typeof(char)) return fromHsl(30f / 360f, 37f / 85f, 7f / 85f);
+        if (type == typeof(char)) return fromHsl(30f / 360f, 7f / 85f, 50f / 85f);
         if (type == typeof(string)) return fromHsl(30f / 360f, 7f / 85f, 75f / 85f);
 
         if (tryGetNumericHsl(type, out var baseHsl, out var t))
